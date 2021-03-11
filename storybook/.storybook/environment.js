ef00c7e1c39b0315894c5d526f6c6c3efb3fe38c
planctus <davidepuntoferrante@gmail.com>
2021-03-11 17:26:34 +0200

    chore: adding jest snapshots

--- storybook/.storybook/environment.js
+++ storybook/.storybook/environment.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
 
-const ecAbsPath = path.resolve(__dirname, "../../src/components");
-const loader = new TwingLoaderFilesystem(ecAbsPath);
+const oeAbsPath = path.resolve(__dirname, "../../src/components");
+const loader = new TwingLoaderFilesystem(oeAbsPath);
 
 // In storybook we get this returned as an instance of
 // TWigLoaderNull, we need to avoid processing this.
 if (typeof loader.addPath === "function") {
   // Add namespace ecl-twig.
-  loader.addPath(ecAbsPath, "oec");
+  loader.addPath(oeAbsPath, "oe");
 }
 
 module.exports = new TwingEnvironment(loader, { autoescape: false });
---
+++ tools/test-utils/README.md
@@ -0,0 +1,1 @@
+# @oe/test-utils
