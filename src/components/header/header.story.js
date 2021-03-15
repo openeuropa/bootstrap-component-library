b71b9e71db516cfbabe560cde556b52c6f3e5926
emeryro <emery.romain@gmail.com>
2021-03-15 10:27:11 +0100

    use modal as component

--- src/components/header/header.story.js
+++ src/components/header/header.story.js
@@ -1,6 +1,101 @@
 import header from "./header.html.twig";
 
 // Controls
+const getArgTypes = (data) => {
+  return {
+    logo: { control: false, table:{ disable:true } },
+    navbar: { control: false, table:{ disable:true } },
+    login: { control: false, table:{ disable:true } },
+    language: { control: false, table:{ disable:true } },
+  }
+}
+
+// Data
+const demoData = {
+  login: {
+    link: {
+      label: 'Log in',
+    },
+    modal: {
+      id: 'loginModal',
+      header: 
+        '<h5 class="modal-title" id="loginModalLabel">Log in</h5>' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
+      ,
+      body: 
+        '<h5>Disclaimer 1</h5>' +
+        '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum accumsan eget orci eu aliquet. Suspendisse potenti. Integer volutpat, nisl et placerat tincidunt</p>' +
+        '<h5>Disclaimer 2</h5>' +
+        '<p>Duis bibendum tristique lobortis. Nam luctus nisi sit amet turpis faucibus, ut dapibus elit sollicitudin. Phasellus a massa magna. Nullam ac imperdiet leo.</p>'
+      ,
+      footer: 
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>' +
+        '<button type="button" class="btn btn-primary">Access EU login</button>'
+      ,
+    },
+  },
+  language: {
+    link: {
+      label: 'English',
+    },
+    modal: {
+      id: 'languageModal',
+      size: 'fullscreen',
+      extra_class: 'oe-language',
+      header: 
+        '<h5 class="modal-title" id="languageeModalLabel">Select your language</h5>' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
+      ,
+      body: 
+        '<div class="container">' +
+          '<div class="row">' +
+            '<div class="col col-lg-8 offset-lg-2">' +
+              '<h5 class="oe-language__title">EU official languages</h5>' +
+            '</div>' +
+          '</div>' +
+          '<div class="row">' +
+            '<div class="col col-lg-4 offset-lg-2">' +
+              '<div class="oe-language__list">' +
+                '<a href="#" class="oe-language__item">Language 01</a>' +
+                '<a href="#" class="oe-language__item">Language 02</a>' +
+                '<a href="#" class="oe-language__item">Language 03</a>' +
+              '</div>' +
+            '</div>' +
+            '<div class="col col-lg-4">' +
+              '<div class="oe-language__list">' +
+                '<a href="#" class="oe-language__item">Language 04</a>' +
+                '<a href="#" class="oe-language__item">Language 05</a>' +
+                '<a href="#" class="oe-language__item">Language 06</a>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+          '<div class="row">' +
+            '<div class="col col-lg-8 offset-lg-2">' +
+              '<h5 class="oe-language__title">Non EU languages</h5>' +
+            '</div>' +
+          '</div>' +
+          '<div class="row">' +
+            '<div class="col col-lg-4 offset-lg-2">' +
+              '<div class="oe-language__list">' +
+                '<a href="#" class="oe-language__item">Language 07</a>' +
+                '<a href="#" class="oe-language__item">Language 08</a>' +
+              '</div>' +
+            '</div>' +
+            '<div class="col col-lg-4">' +
+              '<div class="oe-language__list">' +
+                '<a href="#" class="oe-language__item">Language 09</a>' +
+                '<a href="#" class="oe-language__item">Language 10</a>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      ,
+      footer: 
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>'
+      ,
+    },
+  }
+}
 
 // Stories
 export default {
@@ -10,7 +105,8 @@ export default {
 export const Header = (args) => header(args);
 
 Header.storyName = "default";
-Header.argTypes = {};
+Header.args = demoData,
+Header.argTypes = getArgTypes();
 Header.parameters = {
   cssresources: [
     {
@@ -22,7 +118,7 @@ Header.parameters = {
   ],
 };
 
-const HeaderECLArgs = {
+const headerECLData = {
   logo: {
     src: "https://ec.europa.eu/info/sites/info/themes/europa/images/svg/logo/logo--en.svg",
     width: "240",
@@ -31,11 +127,14 @@ const HeaderECLArgs = {
   navbar: {
     color_set: "dark",
   },
+  login: demoData.login,
+  language: demoData.language,
 };
-export const HeaderECL = (args) => header(HeaderECLArgs);
+export const HeaderECL = (args) => header(args);
 
 HeaderECL.storyName = "ECL branding";
-HeaderECL.argTypes = {};
+HeaderECL.args = headerECLData,
+HeaderECL.argTypes = getArgTypes();
 HeaderECL.parameters = {
   cssresources: [
     {
