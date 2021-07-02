0f11d06915b1551c00a24df52c14eaab6cc474d3
planctus <davidepuntoferrante@gmail.com>
2021-07-02 16:17:42 +0300

    chore(builder): index.esm.js in ecl theme restored

---
+++ src/themes/ecl/index.esm.js
@@ -0,0 +1,34 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.0.0): index.esm.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from "@openeuropa/bcl-bootstrap/js/src/alert";
+import Button from "@openeuropa/bcl-bootstrap/js/src/button";
+import Carousel from "@openeuropa/bcl-bootstrap/js/src/carousel";
+import Collapse from "@openeuropa/bcl-bootstrap/js/src/collapse";
+import Dropdown from "@openeuropa/bcl-bootstrap/js/src/dropdown";
+import Modal from "@openeuropa/bcl-bootstrap/js/src/modal";
+import Offcanvas from "@openeuropa/bcl-bootstrap/js//src/offcanvas";
+import Popover from "@openeuropa/bcl-bootstrap/js/src/popover";
+import ScrollSpy from "@openeuropa/bcl-bootstrap/js/src/scrollspy";
+import Tab from "@openeuropa/bcl-bootstrap/js/src/tab";
+import Toast from "@openeuropa/bcl-bootstrap/js/src/toast";
+import Tooltip from "@openeuropa/bcl-bootstrap/js/src/tooltip";
+
+export {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip,
+};
