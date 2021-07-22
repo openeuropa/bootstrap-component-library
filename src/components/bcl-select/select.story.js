2e9beb3778a1c8362c78723afdb39102be4025ab
tibi2303 <36895476+tibi2303@users.noreply.github.com>
2021-07-22 17:11:01 +0300

    feat(multiselect): Added multiselect - FRONT-2351 (#2)
    * Added multiselect
    
    * Prettier code
    
    * PR feedback and refactor
    
    * Prettier
    
    * Added error state on multiselect
    
    * Prettier
    
    * Prettier
    
    * Prettier
    
    * PR feedback
    
    * PR feedback
    
    * stylelint scss multiselect
    
    * stylelint scss multiselect
    
    * Prettier and improvements
    
    * Lint scss multiselect
    
    * Improvements
    
    * Prettier
    
    * Improvements
    
    * Prettier
    
    * feat(multiselect): integrating slimselect npm package
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>
    Co-authored-by: Alexis Gaillard <a@bypopcorn.com>

--- src/components/bcl-select/select.story.js
+++ src/components/bcl-select/select.story.js
@@ -1,6 +1,7 @@
 import { withDesign } from "storybook-addon-designs";
 import { getFormControls } from "@openeuropa/bcl-story-utils";
 import demoData from "@openeuropa/bcl-data-select/data.js";
+import demoMultiData from "@openeuropa/bcl-data-select/dataMultiselect.js";
 import select from "./select.html.twig";
 import drupalAttribute from "drupal-attribute";
 
@@ -38,27 +39,50 @@ const applyArgs = (data, args) => {
 export default {
   title: "Components/Forms/Select",
   decorators: [withDesign],
-  parameters: {
-    design: [
-      {
-        name: "Wireframe",
-        type: "figma",
-        url: "https://www.figma.com/file/Ug1zpiazvPT8la7ySWZy47/OEL-Wireframe-kit?node-id=2695%3A58",
-      },
-      {
-        name: "Mockup",
-        type: "figma",
-        url: "https://www.figma.com/file/MPL8vE2LF4RQKLY4HcxHTs/OEL-Mockup-kit?node-id=53%3A24",
-      },
-      {
-        name: "Bootstrap docs",
-        type: "iframe",
-        url: "https://getbootstrap.com/docs/5.0/forms/select/",
-      },
-    ],
-  },
 };
 
 export const Default = (args) => select(applyArgs(demoData, args));
 
 Default.argTypes = getArgTypes(demoData, "select");
+Default.parameters = {
+  design: [
+    {
+      name: "Wireframe",
+      type: "figma",
+      url: "https://www.figma.com/file/Ug1zpiazvPT8la7ySWZy47/OEL-Wireframe-kit?node-id=2695%3A58",
+    },
+    {
+      name: "Mockup",
+      type: "figma",
+      url: "https://www.figma.com/file/MPL8vE2LF4RQKLY4HcxHTs/OEL-Mockup-kit?node-id=53%3A24",
+    },
+    {
+      name: "Bootstrap docs",
+      type: "iframe",
+      url: "https://getbootstrap.com/docs/5.0/forms/select/",
+    },
+  ],
+};
+
+export const Multiselect = (args) => select(applyArgs(demoMultiData, args));
+
+Multiselect.argTypes = getArgTypes(demoMultiData, "multiselect");
+Multiselect.parameters = {
+  design: [
+    {
+      name: "Wireframe",
+      type: "figma",
+      url: "https://www.figma.com/file/Ug1zpiazvPT8la7ySWZy47/OEL-Wireframe-kit?node-id=2695%3A58",
+    },
+    {
+      name: "Mockup",
+      type: "figma",
+      url: "https://www.figma.com/file/MPL8vE2LF4RQKLY4HcxHTs/OEL-Mockup-kit?node-id=53%3A24",
+    },
+    {
+      name: "Slim select docs",
+      type: "iframe",
+      url: "https://slimselectjs.com/",
+    },
+  ],
+};
