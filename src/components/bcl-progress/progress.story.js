cc5bbef7393daf328d1d21667663d0b21cda0d7f
planctus <davidepuntoferrante@gmail.com>
2021-07-30 15:33:12 +0300

    chore(storybook): Refactoring select controls options, removing argType.defaultValue (#25)
    * chore(storybook): Refactoring select controls options, removing argType.defaultValue
    
    * chore: reverting unrelated changes in badge.html.twig

--- src/components/bcl-progress/progress.story.js
+++ src/components/bcl-progress/progress.story.js
@@ -3,12 +3,20 @@ import demoData from "@openeuropa/bcl-data-progress/data.js";
 import progress from "./progress.html.twig";
 import { getVariants } from "@openeuropa/bcl-story-utils";
 
+const getArgs = (data) => {
+  return {
+    striped: false,
+    animated: false,
+    variant: "",
+    progress: data.progress,
+  };
+};
+
 const getArgTypes = (data) => {
   return {
     striped: {
       type: { name: "boolean" },
       description: "Striped bar",
-      defaultValue: false,
       table: {
         type: { summary: "boolean" },
         defaultValue: { summary: "false" },
@@ -18,7 +26,6 @@ const getArgTypes = (data) => {
     animated: {
       type: { name: "boolean" },
       description: "Animated bar",
-      defaultValue: false,
       table: {
         type: { summary: "boolean" },
         defaultValue: { summary: "false" },
@@ -28,11 +35,7 @@ const getArgTypes = (data) => {
     variant: {
       type: { name: "select" },
       description: "Variant of the progress",
-      defaultValue: "",
-      control: {
-        type: "select",
-        options: getVariants(false),
-      },
+      options: getVariants(false),
       table: {
         type: { summary: "string" },
         defaultValue: { summary: "" },
@@ -42,7 +45,6 @@ const getArgTypes = (data) => {
     progress: {
       type: { name: "number" },
       description: "Progress of the bar",
-      defaultValue: data.progress,
       table: {
         type: { summary: "number" },
         defaultValue: { summary: "" },
@@ -88,4 +90,5 @@ export default {
 
 export const Default = (args) => progress(applyArgs(demoData, args));
 
+Default.args = getArgs(demoData);
 Default.argTypes = getArgTypes(demoData);
