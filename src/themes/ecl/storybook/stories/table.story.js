df76989b07de9735f42da05dfa5010d6d5aac1fe
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-08-09 14:55:50 +0300

    Change template in custom story

--- src/themes/ecl/storybook/stories/table.story.js
+++ src/themes/ecl/storybook/stories/table.story.js
@@ -1,8 +1,6 @@
 import { withDesign } from "storybook-addon-designs";
 import demoData from "@openeuropa/bcl-data-table/data.js";
-import table from "@openeuropa/bcl-table/table.html.twig";
-import { getVariants } from "@openeuropa/bcl-story-utils";
-import drupalAttribute from "drupal-attribute";
+import table from "../../custom-templates/bcl-table/table.html.twig";
 
 const getArgs = (data) => {
   return {
