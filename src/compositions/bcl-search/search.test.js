e8116d2c34b9cbb5a1231bf3b352d0cd8f7cb031
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-08-11 16:02:41 +0300

    Improvements

--- src/compositions/bcl-search/search.test.js
+++ src/compositions/bcl-search/search.test.js
@@ -2,7 +2,7 @@ import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
 import demoData from "@openeuropa/bcl-search/dataSearch";
 
 const template = "@oe-bcl/bcl-search/search.html.twig";
-const render = (params) => renderTwigFileAsNode(template, params, true);
+const render = (params) => renderTwigFileAsNode(template, params);
 
 describe("OE - Search", () => {
   test(`renders correctly`, () => {
