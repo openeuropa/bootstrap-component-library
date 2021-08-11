09e59a17ffc9fb499e4cd45e2368e621c0944453
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-08-11 15:57:52 +0300

    Added test for search

---
+++ src/compositions/bcl-search/search.test.js
@@ -0,0 +1,13 @@
+import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
+import demoData from "@openeuropa/bcl-search/dataSearch";
+
+const template = "@oe-bcl/bcl-search/search.html.twig";
+const render = (params) => renderTwigFileAsNode(template, params, true);
+
+describe("OE - Search", () => {
+  test(`renders correctly`, () => {
+    expect.assertions(1);
+
+    return expect(render(demoData)).resolves.toMatchSnapshot();
+  });
+});
