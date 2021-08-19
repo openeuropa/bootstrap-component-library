e962dc34c9e6d99d600216fe3d15851ee66a7ba0
tibi2303 <36895476+tibi2303@users.noreply.github.com>
2021-08-19 17:38:24 +0300

    feat(search-improvements): Improvements on search - FRONT-2785 (#45)

--- src/compositions/bcl-search/search.story.js
+++ src/compositions/bcl-search/search.story.js
@@ -6,9 +6,28 @@ export default {
   title: "Compositions/Search",
 };
 
+const scriptInit = (story) => {
+  const demo = story();
+  return `
+    <script>
+      var badges = document.querySelectorAll(".badge");
+      badges.forEach(element => {
+        var close = element.getElementsByTagName('span')[0];
+        close.addEventListener('click', event => {
+          close.parentElement.remove();
+        })
+      });
+    </script>
+  ${demo}`;
+};
+
 const correctPaths = (data) => {
   data.data.filter_button.icon.path = defaultSprite;
+  data.data.badges.forEach((badge) => {
+    badge.icons_path = defaultSprite;
+  });
   return data;
 };
 
 export const Default = () => search(correctPaths(demoData));
+Default.decorators = [scriptInit];
