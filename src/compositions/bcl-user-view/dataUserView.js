462790dd36d027386a442ab754c08086132a4145
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-09-01 10:26:50 +0300

    User view: Responsive

--- src/compositions/bcl-user-view/dataUserView.js
+++ src/compositions/bcl-user-view/dataUserView.js
@@ -97,14 +97,14 @@ module.exports = {
         gutter: 2,
       },
       extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
-      extra_classes_body: "pt-0",
+      extra_classes_body: "pt-md-0 px-0 px-md-3",
       title: {
         content: "Stefan Mayer",
         tag: "h2",
       },
       content:
         "<div class='mb-4 mt-4'><span class='text-muted me-3'>DG Test</span><span class='text-muted me-3'>Director</span></div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ultricies massa, a dapibus libero porta nec. Sed facilisis dictum vestibulum. Fusce commodo hendrerit diam, pretium tempus leo varius sit amet. Etiam interdum, orci at sagittis luctus, lorem libero tempus mauris, a fermentum libero orci semper lacus. Duis tristique fringilla magna, eu egestas dolor molestie non. </p>",
-      attributes: new drupalAttribute().addClass(["border-0", "mb-5"]),
+      attributes: new drupalAttribute().addClass(["border-0"]),
       image: {
         path: "https://picsum.photos/200/200?random=6",
         alt: "alt img",
@@ -114,110 +114,7 @@ module.exports = {
     editButton: {
       label: "Manage profile",
       variant: "secondary",
-    },
-    personalInfoBasic: {
-      borderless: true,
-      attributes: new drupalAttribute().addClass("w-100"),
-      rows: [
-        {
-          cells: [
-            {
-              content: "Gender :",
-            },
-            {
-              content: "Male",
-            },
-          ],
-        },
-        {
-          cells: [
-            {
-              content: "Nationality :",
-            },
-            {
-              content: "German",
-            },
-          ],
-        },
-        {
-          cells: [
-            {
-              content: "Date of birth :",
-            },
-            {
-              content: "22/01/1972",
-            },
-          ],
-        },
-      ],
-    },
-    personalInfoLocation: {
-      borderless: true,
-      attributes: new drupalAttribute().addClass("w-100"),
-      rows: [
-        {
-          cells: [
-            {
-              content: "Country :",
-            },
-            {
-              content: "Germany",
-            },
-          ],
-        },
-        {
-          cells: [
-            {
-              content: "City :",
-            },
-            {
-              content: "Düsseldorf",
-            },
-          ],
-        },
-      ],
-    },
-    professionalInfo: {
-      borderless: true,
-      attributes: new drupalAttribute().addClass("w-100"),
-      rows: [
-        {
-          cells: [
-            {
-              content: "Organisation :",
-            },
-            {
-              content: "DG Test",
-            },
-          ],
-        },
-        {
-          cells: [
-            {
-              content: "Position :",
-            },
-            {
-              content: "Director",
-            },
-          ],
-        },
-      ],
-    },
-    professionalLanguages: {
-      borderless: true,
-      attributes: new drupalAttribute().addClass("w-100"),
-      rows: [
-        {
-          cells: [
-            {
-              content: "Working languages :",
-            },
-            {
-              content: "English, German",
-            },
-          ],
-        },
-      ],
+      attributes: new drupalAttribute().addClass(["w-100", "mb-3"]),
     },
   },
 };
