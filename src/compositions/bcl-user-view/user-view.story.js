27a13b0fc29abbdda8353d0efa3b1e1fee9011a2
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-09-02 11:07:59 +0300

    User view: Added figma link

--- src/compositions/bcl-user-view/user-view.story.js
+++ src/compositions/bcl-user-view/user-view.story.js
@@ -3,6 +3,15 @@ import userView from "@openeuropa/bcl-user-view/user-view.html.twig";
 
 export default {
   title: "Compositions/Users/View",
+  parameters: {
+    design: [
+      {
+        name: "Wireframe",
+        type: "figma",
+        url: "https://www.figma.com/file/NQlGvTiTXZYN8TwY2Ur5EI/BCL-Features?node-id=1126%3A38732",
+      },
+    ],
+  },
 };
 
 export const Default = () => userView(demoData);
