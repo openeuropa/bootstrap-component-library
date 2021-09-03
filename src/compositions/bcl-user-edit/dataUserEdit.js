91eb5ff27e40541d260c8fcef7fb4740f9821753
Theo Suja <theo.suja@twtspain.com>
2021-09-03 17:04:57 +0200

    FRONT-3040: Create Privacy Settings tab's content.

--- src/compositions/bcl-user-edit/dataUserEdit.js
+++ src/compositions/bcl-user-edit/dataUserEdit.js
@@ -115,6 +115,176 @@ module.exports = {
             .setAttribute("aria-controls", "privacy"),
         },
       ],
-    }
+      privacySettings: {
+        attributes: new drupalAttribute()
+          .addClass("mb-3")
+          .setAttribute("novalidate", true)
+          .setAttribute("onsubmit", "return false;"),
+        submit: {
+          label: "Save",
+          type: "submit",
+          variant: "primary",
+          wrapper: "mt-4 d-grid d-sm-block",
+        },
+        items: [
+          [
+            {
+              label: "Who can consult my data",
+              wrapper_classes: "col-sm-6 col-lg-3",
+              type: "select",
+              size: "md",
+              helper_text: "Helper text",
+              helper_text_id: "helperText",
+              options: [
+                { value: 1, label: "Anonymous users" },
+                { value: 2, label: "Nobody" },
+                { label: "Authenticated users only", selected: true },
+              ],
+            },
+          ],
+        ],
+      },
+      publicProfileContent: {
+        attributes: new drupalAttribute()
+          .addClass(["row", "mb-3"])
+          .setAttribute("novalidate", true)
+          .setAttribute("onsubmit", "return false;"),
+        submit: {
+          label: "Save",
+          type: "submit",
+          variant: "primary",
+          wrapper: "mt-4 d-grid d-sm-block",
+        },
+        items: [
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Picture",
+              id: "picture",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Position",
+              id: "position",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "City",
+              id: "city",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "First name",
+              id: "firstName",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Gender",
+              id: "gender",
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Nationality",
+              id: "nationality",
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Last name",
+              id: "lastName",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Working languages",
+              id: "workingLanguages",
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "mb-3 col-sm-6 col-md-4",
+              required: true,
+              label: "Date of birth",
+              id: "dateOfBirth",
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "col-sm-6 col-md-4",
+              required: true,
+              label: "Organisation",
+              id: "organisation",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "col-sm-6 col-md-4",
+              required: true,
+              label: "Country",
+              id: "country",
+              checked: true,
+            },
+          ],
+          [
+            {
+              input_type: "checkbox",
+              classes: "col-sm-6 col-md-4",
+              required: true,
+              label: "Bio",
+              id: "bio",
+              checked: true,
+            },
+          ],
+        ],
+      },
+      deleteAccount: {
+        attributes: new drupalAttribute()
+          .setAttribute("novalidate", true)
+          .setAttribute("onsubmit", "return false;"),
+        submit: {
+          label: "Delete my account",
+          type: "submit",
+          variant: "danger",
+          wrapper: "mt-4 d-grid d-sm-block",
+        },
+      },
+    },
   },
 };
