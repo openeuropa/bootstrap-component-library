25595961bec12d44ecbad1e076a2e42a8303aca4
Theo Suja <theo.suja@twtspain.com>
2021-09-14 18:20:32 +0200

    FRONT-3136: Apply requested changes.

--- src/compositions/bcl-links-block/dataLinksBlock.js
+++ src/compositions/bcl-links-block/dataLinksBlock.js
@@ -1,21 +1,23 @@
 const drupalAttribute = require("drupal-attribute");
 
 module.exports = {
-  data: {
-    title: "More information",
-    links: [
-      {
-        path: "#",
-        label: "European Commission",
-      },
-      {
-        path: "#",
-        label: "Priorities",
-      },
-      {
-        path: "#",
-        label: "Jobs, Growth and Investment",
-      },
-    ],
+  title: {
+    label: "More information",
+    classes: "",
+    tag: "h3",
   },
+  links: [
+    {
+      path: "#",
+      label: "European Commission",
+    },
+    {
+      path: "#",
+      label: "Priorities",
+    },
+    {
+      path: "#",
+      label: "Jobs, Growth and Investment",
+    },
+  ],
 };
