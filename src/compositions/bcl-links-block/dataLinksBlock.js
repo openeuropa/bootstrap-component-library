f8a2634c2a1c830f5e0274dfa42f46fd0d4d55f4
Theo SUJA <theo.suja@twtspain.com>
2021-09-20 17:31:33 +0200

    feat(links block): Add composition of links block - Front 3136 (#61)

---
+++ src/compositions/bcl-links-block/dataLinksBlock.js
@@ -0,0 +1,21 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  variant: "gray",
+  direction: "vertical",
+  title: "More information",
+  links: [
+    {
+      path: "/example.html",
+      label: "European Commission",
+    },
+    {
+      path: "/example.html",
+      label: "Priorities",
+    },
+    {
+      path: "/example.html",
+      label: "Jobs, Growth and Investment",
+    },
+  ],
+};
