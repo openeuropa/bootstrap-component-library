182b3444ad80239693a7c2705be10a9a9cba32f6
planctus <davidepuntoferrante@gmail.com>
2021-10-05 13:17:30 +0300

    feat(tooltip): Adding control for tooltip position in button and link - FRONT-3172 (#68)
    * feat(tooltip): Adding control for tooltip position in button and link - front-3172
    
    * feat(tooltip): Removing bg from the demo - front-3172
    
    Co-authored-by: Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>

--- src/data/link/tooltipData.js
+++ src/data/link/tooltipData.js
@@ -5,7 +5,6 @@ module.exports = {
   path: "/example.html",
   attributes: new drupalAttribute()
     .setAttribute("title", "This is a tooltip")
-    .setAttribute("data-bs-placement", "bottom")
     .setAttribute("data-bs-toggle", "tooltip")
     .setAttribute("role", "button"),
 };
