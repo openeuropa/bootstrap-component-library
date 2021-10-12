190f269b62dae803308b2524c8e87bc6f522bcfd
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-10-12 13:07:15 +0300

    Tabs: New style and improvements.

---
+++ src/data/navigation/dataCustom.js
@@ -0,0 +1,64 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  pills: true,
+  nav: true,
+  attributes: new drupalAttribute().addClass("bcl__nav"),
+  items: [
+    {
+      label: "I'm a link 1",
+      path: "/example.html",
+    },
+    {
+      label: "I'm a link 2",
+      path: "/example.html",
+    },
+    {
+      label: "I'm a link 3",
+      path: "/example.html",
+    },
+    {
+      label: "I'm a link active",
+      path: "/example.html",
+      active: true,
+    },
+    {
+      label: "I'm a link disabled",
+      path: "/example.html",
+      disabled: true,
+    },
+    {
+      id: "dropdown-1",
+      link: true,
+      dropdown: true,
+      trigger: {
+        label: "Dropdown Toggle",
+        path: "#",
+        attributes: new drupalAttribute().addClass("nav-link"),
+      },
+      items: [
+        {
+          label: "I'm a link",
+          path: "/example.html",
+        },
+        {
+          label: "I'm a button",
+          button: true,
+        },
+        {
+          divider: true,
+        },
+        {
+          label: "I'm a disabled button",
+          button: true,
+          disabled: true,
+        },
+        {
+          label: "I'm a active button",
+          button: true,
+          active: true,
+        },
+      ],
+    },
+  ],
+};
