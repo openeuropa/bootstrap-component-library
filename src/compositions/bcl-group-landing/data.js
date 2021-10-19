7f5ede6fc083c94ecae1258d46b168bed8b8ed8e
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2021-10-19 12:27:35 +0300

    Group: Added composition.

---
+++ src/compositions/bcl-group-landing/data.js
@@ -0,0 +1,476 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  data: {
+    banner: {
+      image: {
+        path: "https://picsum.photos/seed/1002/1100/255",
+        alt: "alt img",
+        class: "w-100",
+      },
+      title: "Group Name",
+      actions: [
+        {
+          type: "link",
+          label: "Leave group",
+          path: "/example.html",
+          attributes: new drupalAttribute().addClass([
+            "d-inline-block",
+            "me-2",
+            "p-2",
+          ]),
+        },
+        {
+          type: "dropdown",
+          id: "dropdown-1",
+          remove_wrapper: true,
+          trigger: {
+            label: "Create content",
+            path: "#",
+            attributes: new drupalAttribute().addClass("d-inline-block"),
+          },
+          items: [
+            {
+              label: "I'm a link",
+              path: "#",
+            },
+            {
+              label: "I'm a button",
+              button: true,
+            },
+            {
+              divider: true,
+            },
+            {
+              label: "I'm a disabled button",
+              button: true,
+              disabled: true,
+            },
+            {
+              label: "I'm a active button",
+              button: true,
+              active: true,
+            },
+          ],
+        },
+      ],
+      content:
+        "<h2 class='mb-0'>Group name</h2><div><a href='#'>Leave group</a><button></div>",
+      extra_classes_body: "p-3-5",
+      attributes: new drupalAttribute().addClass("border-0"),
+    },
+    navigation: {
+      nav: true,
+      tabs: true,
+      attributes: new drupalAttribute().addClass(["bcl__nav", "bg-gray"]),
+      items: [
+        {
+          label: "Home",
+          path: "/example.html",
+          active: true,
+        },
+        {
+          label: "Members",
+          path: "/example.html",
+        },
+        {
+          label: "Content",
+          path: "/example.html",
+        },
+        {
+          id: "dropdown-1",
+          link: true,
+          dropdown: true,
+          trigger: {
+            label: "Links",
+            path: "#",
+            attributes: new drupalAttribute().addClass("nav-link"),
+          },
+          items: [
+            {
+              label: "I'm a link",
+              path: "/example.html",
+            },
+            {
+              label: "I'm a button",
+              button: true,
+            },
+            {
+              divider: true,
+            },
+            {
+              label: "I'm a disabled button",
+              button: true,
+              disabled: true,
+            },
+            {
+              label: "I'm a active button",
+              button: true,
+              active: true,
+            },
+          ],
+        },
+        {
+          label: "Link",
+          path: "/example.html",
+        },
+      ],
+    },
+    banner_description: {
+      metas: [
+        {
+          label: "Public group",
+          icon: {
+            name: "key-fill",
+            size: "xs",
+            attributes: new drupalAttribute().addClass([
+              "me-2",
+              "align-top",
+              "mt-1",
+            ]),
+          },
+        },
+        {
+          label: "22 Members",
+          icon: {
+            name: "people-fill",
+            size: "xs",
+            attributes: new drupalAttribute().addClass([
+              "me-2",
+              "align-top",
+              "mt-1",
+            ]),
+          },
+        },
+      ],
+      content:
+        "<p class='mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ultricies massa, a dapibus libero porta nec. Sed facilisis dictum vestibulum. Fusce commodo hendrerit diam, pretium tempus leo varius sit amet. Etiam interdum, orci at sagittis luctus, lorem libero tempus mauris, a fermentum libero orci semper lacus. Duis tristique fringilla magna, eu egestas dolor molestie non.</p>",
+    },
+    contact: {
+      title: "Contact information",
+      profiles: [
+        {
+          picture: {
+            path: "https://picsum.photos/50/50",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+      ],
+    },
+    recent: {
+      title: "Recent activities",
+      block_data: {
+        activities: [
+          {
+            image:
+              "<img class='rounded-circle me-3-5' src='https://picsum.photos/50/50?random=1'>",
+            content:
+              "<a href='/example.html' class='fw-bold text-decoration-none'>Adjur Tichar</a> commented on the article <a href='#' class='text-decoration-none'>SpaceX satellites spotted: how good is this for our health and environment?</a>",
+            meta: [
+              {
+                icon: {
+                  name: "clock",
+                  size: "xs",
+                  attributes: new drupalAttribute().addClass([
+                    "me-2",
+                    "align-top",
+                    "mt-1",
+                  ]),
+                },
+                label: "8 hours ago",
+              },
+            ],
+          },
+          {
+            image:
+              "<img class='rounded-circle me-3-5' src='https://picsum.photos/50/50?random=2'>",
+            content:
+              "<a href='/example.html' class='fw-bold text-decoration-none'>Adjur Tichar</a> commented on the article <a href='#' class='text-decoration-none'>SpaceX satellites spotted: how good is this for our health and environment?</a>",
+            meta: [
+              {
+                icon: {
+                  name: "clock",
+                  size: "xs",
+                  attributes: new drupalAttribute().addClass([
+                    "me-2",
+                    "align-top",
+                    "mt-1",
+                  ]),
+                },
+                label: "8 hours ago",
+              },
+            ],
+          },
+          {
+            image:
+              "<img class='rounded-circle me-3-5' src='https://picsum.photos/50/50?random=3'>",
+            content:
+              "<a href='/example.html' class='fw-bold text-decoration-none'>Adjur Tichar</a> commented on the article <a href='#' class='text-decoration-none'>SpaceX satellites spotted: how good is this for our health and environment?</a>",
+            meta: [
+              {
+                icon: {
+                  name: "clock",
+                  size: "xs",
+                  attributes: new drupalAttribute().addClass([
+                    "me-2",
+                    "align-top",
+                    "mt-1",
+                  ]),
+                },
+                label: "8 hours ago",
+              },
+            ],
+          },
+          {
+            image:
+              "<img class='rounded-circle me-3-5' src='https://picsum.photos/50/50?random=4'>",
+            content:
+              "<a href='/example.html' class='fw-bold text-decoration-none'>Steve Di Marzio</a> published the article <a href='/example.html' class='text-decoration-none'>SpaceX satellites spotted: how good is this for our health and environment?</a> in <a href='/example.html' class='text-decoration-none'>News</a>",
+            meta: [
+              {
+                icon: {
+                  name: "clock",
+                  size: "xs",
+                  attributes: new drupalAttribute().addClass([
+                    "me-2",
+                    "align-top",
+                    "mt-1",
+                  ]),
+                },
+                label: "8 hours ago",
+              },
+            ],
+          },
+          {
+            image:
+              "<img class='rounded-circle me-3-5' src='https://picsum.photos/50/50?random=5'>",
+            content:
+              "<a href='/example.html' class='fw-bold text-decoration-none'>Adjur Tichar</a> commented on the article <a href='/example.html' class='text-decoration-none'>SpaceX satellites spotted: how good is this for our health and environment?</a>",
+            meta: [
+              {
+                icon: {
+                  name: "clock",
+                  size: "xs",
+                  attributes: new drupalAttribute().addClass([
+                    "me-2",
+                    "align-top",
+                    "mt-1",
+                  ]),
+                },
+                label: "8 hours ago",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    members: {
+      title: "Latest members",
+      profiles: [
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=6",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=7",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=8",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=9",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=10",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+        {
+          picture: {
+            path: "https://picsum.photos/50/50?random=11",
+            alt: "Stefan Mayer profile picture",
+          },
+          name: {
+            text: "Stefan Mayer",
+            link: "/example.html",
+          },
+          infos: ["DG Test", "Director"],
+        },
+      ],
+      more_link: {
+        label: "See all group members",
+        path: "/example.html",
+        icon_position: "after",
+        icon: {
+          name: "chevron-right",
+          size: "xs",
+        },
+        attributes: new drupalAttribute().addClass(["py-2", "d-inline-block"]),
+      },
+    },
+    contributions: {
+      title: "Latest contributions",
+      more_link: {
+        label: "See all group contributions",
+        path: "/example.html",
+        icon_position: "after",
+        icon: {
+          name: "chevron-right",
+          size: "xs",
+        },
+        attributes: new drupalAttribute().addClass([
+          "py-3",
+          "d-inline-block",
+          "mb-4",
+        ]),
+      },
+      card_layout: {
+        type: "grid",
+        responsive_colums: 3,
+        responsiveness: "md",
+        attributes: new drupalAttribute().addClass("mb-3"),
+        items: [
+          {
+            title: {
+              content: "Content title",
+            },
+            badges: [
+              {
+                label: "Label",
+              },
+              {
+                label: "Label",
+              },
+            ],
+            text: {
+              content:
+                "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+              classes: "mb-2",
+            },
+            content:
+              "<div class='mb-4 mt-4'><span class='text-muted me-3'>8 hours ago</span><span class='text-muted me-3'>1 comment</span></div>",
+            image: {
+              path: "https://picsum.photos/seed/1002/600/400",
+              alt: "alt img",
+              position: "top",
+            },
+            wrapper_class: "col",
+          },
+          {
+            title: {
+              content: "Content title",
+            },
+            badges: [
+              {
+                label: "Label",
+              },
+              {
+                label: "Label",
+              },
+            ],
+            text: {
+              content:
+                "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+              classes: "mb-2",
+            },
+            content:
+              "<div class='mb-4 mt-4'><span class='text-muted me-3'>8 hours ago</span><span class='text-muted me-3'>1 comment</span></div>",
+            image: {
+              path: "https://picsum.photos/seed/1002/600/400",
+              alt: "alt img",
+              position: "top",
+            },
+            wrapper_class: "col",
+          },
+          {
+            title: {
+              content: "Content title",
+            },
+            badges: [
+              {
+                label: "Label",
+              },
+            ],
+            text: {
+              content:
+                "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+              classes: "mb-2",
+            },
+            content:
+              "<div class='mb-4 mt-4'><span class='text-muted me-3'>8 hours ago</span><span class='text-muted me-3'>1 comment</span></div>",
+            image: {
+              path: "https://picsum.photos/seed/1002/600/400",
+              alt: "alt img",
+              position: "top",
+            },
+            wrapper_class: "col",
+          },
+        ],
+      },
+    },
+  },
+};
---
+++ src/compositions/bcl-group-landing/group-landing.story.js
@@ -0,0 +1,70 @@
+import { withDesign } from "storybook-addon-designs";
+import headerData from "@openeuropa/bcl-data-header/data.js";
+import footerData from "@openeuropa/bcl-data-footer/data.js";
+import GroupLanding from "@openeuropa/bcl-group-landing/group-landing.html.twig";
+import demoData from "@openeuropa/bcl-group-landing/data";
+import defaultSprite from "@openeuropa/bcl-bootstrap/bootstrap-icons.svg";
+
+demoData.data.header = headerData;
+demoData.data.footer = footerData;
+
+export default {
+  title: "Compositions/Group Landing",
+  decorators: [withDesign],
+  parameters: {
+    layout: "fullscreen",
+    design: [
+      {
+        name: "Mockup - Footer Desktop",
+        type: "figma",
+        url: "https://www.figma.com/file/7aJedLkk8hiDoD3RcxTnQi/BCL-Starter-kit?node-id=5118%3A65509",
+      },
+      {
+        name: "Mockup - Footer Mobile",
+        type: "figma",
+        url: "https://www.figma.com/file/7aJedLkk8hiDoD3RcxTnQi/BCL-Starter-kit?node-id=5222%3A62722",
+      },
+    ],
+    controls: {
+      disable: true,
+    },
+  },
+};
+
+const correctPaths = (data) => {
+  data.data.header.head.navigation.items.forEach((item) => {
+    if (item.icon) {
+      item.icon.path = defaultSprite;
+    }
+  });
+  data.data.header.navbar.form.submit.icon.path = defaultSprite;
+  data.data.header.breadcrumbs.icons_path = defaultSprite;
+  data.data.footer.rows.forEach((row) => {
+    row.cols.forEach((col) => {
+      if (col.items) {
+        col.items.forEach((item) => {
+          if (item.type == "links") {
+            item.links.forEach((link) => {
+              if (link.icon) {
+                link.icon.path = defaultSprite;
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+  data.data.recent.block_data.activities.forEach((activity) => {
+    activity.meta.forEach((meta) => {
+      meta.icon.path = defaultSprite;
+    });
+  });
+  data.data.banner_description.metas.forEach((meta) => {
+    meta.icon.path = defaultSprite;
+  });
+  data.data.contributions.more_link.icon.path = defaultSprite;
+  data.data.members.more_link.icon.path = defaultSprite;
+  return data;
+};
+
+export const Default = () => GroupLanding(correctPaths(demoData));
