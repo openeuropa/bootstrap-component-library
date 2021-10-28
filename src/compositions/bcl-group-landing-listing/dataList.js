db91e82fdc4c5095555bd3464b9b18db56b9dd33
tibi2303 <36895476+tibi2303@users.noreply.github.com>
2021-10-28 10:41:27 +0300

    feat(group): Added composition. - FRONT-3269 (#99)

---
+++ src/compositions/bcl-group-landing-listing/dataList.js
@@ -0,0 +1,437 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  data: {
+    main_title: "Groups (38)",
+    offcanvas: {
+      id: "bcl-offcanvas",
+      attributes: new drupalAttribute().addClass(
+        "bcl-offcanvas offcanvas-start"
+      ),
+      extra_classes_close: "d-lg-none",
+      extra_classes_header: "pb-2-5 p-lg-0",
+      with_body_scroll: true,
+      title: {
+        label: "Filter options",
+        heading: 4,
+        extra_classes: "mb-lg-4",
+        id: "offcanvasExampleLabel",
+      },
+      search_form: {
+        attributes: new drupalAttribute()
+          .setAttribute("novalidate", true)
+          .setAttribute("onsubmit", "return false;"),
+        items: [
+          [
+            {
+              type: "fieldset",
+              classes: "col-sm-10",
+              legend: "Group type",
+              fieldset_classes: "mb-3",
+              legend_classes: "col-form-label mb-2",
+            },
+            {
+              input_type: "checkbox",
+              label: "Public groups",
+              wrapper_classes: "mb-3",
+              checked: true,
+            },
+            {
+              input_type: "checkbox",
+              label: "Restricted groups",
+              wrapper_classes: "mb-3",
+              checked: true,
+            },
+            {
+              input_type: "checkbox",
+              label: "Private groups",
+            },
+          ],
+          [
+            {
+              type: "fieldset",
+              legend: "Creation date (from)",
+              legend_classes: "col-form-label",
+            },
+            {
+              input_type: "date",
+              attributes: new drupalAttribute().addClass("mb-2"),
+            },
+          ],
+          [
+            {
+              type: "fieldset",
+              legend: "Creation date (to)",
+              fieldset_classes: "mb-3",
+              legend_classes: "col-form-label",
+            },
+            {
+              input_type: "date",
+              attributes: new drupalAttribute().addClass("mb-2"),
+            },
+          ],
+        ],
+        submit: {
+          multiple: true,
+          wrapper: "mt-4 d-grid d-md-block",
+          items: [
+            {
+              label: "Refine",
+              type: "submit",
+              variant: "primary",
+              attributes: new drupalAttribute().addClass([
+                "me-md-3",
+                "mb-3-5",
+                "mb-md-0",
+              ]),
+            },
+            {
+              label: "Clear",
+              type: "submit",
+              variant: "light",
+            },
+          ],
+        },
+      },
+    },
+    cards: [
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+        border_bottom: true,
+      },
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+        border_bottom: true,
+      },
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+        border_bottom: true,
+      },
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+        border_bottom: true,
+      },
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+        border_bottom: true,
+      },
+      {
+        horizontal: true,
+        horizontal_grid: {
+          left_col_classes: "col-md-2",
+          right_col_classes: "col-md-10",
+          gutter: 2,
+        },
+        badges: [
+          {
+            label: "Public",
+          },
+        ],
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        extra_classes_body: "pt-0 px-0 px-md-2-5",
+        title: {
+          content: "Group name",
+          classes: "mb-3 h5 text-decoration-none d-block",
+          tag: "a",
+          attributes: [
+            {
+              name: "href",
+              value: "example.html",
+            },
+          ],
+        },
+        text: {
+          content:
+            "Some quick example text to build on the card and title and make up the bulk of the cards content.",
+        },
+        attributes: new drupalAttribute().addClass([
+          "border-0",
+          "mb-4",
+          "mb-md-5",
+        ]),
+        image: {
+          path: "https://picsum.photos/200/200?random=1",
+          alt: "alt img",
+          position: "top",
+          classes: "d-none d-md-block",
+        },
+      },
+    ],
+    pagination: {
+      attributes: new drupalAttribute().addClass(["mt-5"]),
+      alignment: "center",
+      prev: {
+        label: "Previous",
+        path: "#",
+      },
+      next: {
+        label: "Next",
+        path: "#",
+      },
+      items: [
+        {
+          label: "1",
+          path: "#",
+          active: true,
+        },
+        {
+          label: "2",
+          path: "#",
+        },
+        {
+          label: "3",
+          path: "#",
+        },
+      ],
+    },
+    filter_button: {
+      label: "Filter options",
+      type: "button",
+      variant: "light",
+      attributes: new drupalAttribute()
+        .addClass(["d-lg-none", "w-100", "mb-3-5"])
+        .setAttribute("data-bs-toggle", "offcanvas")
+        .setAttribute("data-bs-target", "#bcl-offcanvas")
+        .setAttribute("aria-controls", "bcl-offcanvas"),
+      icon_position: "before",
+      icon: {
+        name: "filter",
+        size: "xs",
+        attributes: new drupalAttribute().addClass(["ms-2"]),
+      },
+    },
+    badges: [
+      {
+        label: "Public",
+        background: "light",
+        dismissible: true,
+        attributes: new drupalAttribute().addClass([
+          "me-2",
+          "text-dark",
+          "mb-2",
+        ]),
+      },
+      {
+        label: "Restricted",
+        background: "light",
+        dismissible: true,
+        attributes: new drupalAttribute().addClass([
+          "me-2",
+          "text-dark",
+          "mb-2",
+        ]),
+      },
+      {
+        label: "Private",
+        background: "light",
+        dismissible: true,
+        attributes: new drupalAttribute().addClass([
+          "me-2",
+          "text-dark",
+          "mb-2",
+        ]),
+      },
+    ],
+    sort_select: {
+      label: "Sort&nbsp;by",
+      attributes: new drupalAttribute().addClass("mb-4 mb-md-0 ms-md-4"),
+      options: [
+        { value: 1, label: "a select option" },
+        { value: 2, label: "another select option" },
+        { value: 3, label: "another option" },
+        { value: 4, label: "last option" },
+        { label: "Name", selected: true },
+      ],
+    },
+  },
+};
---
+++ src/compositions/bcl-group-landing/group-landing.story.js
@@ -0,0 +1,65 @@
+import { withDesign } from "storybook-addon-designs";
+import headerData from "@openeuropa/bcl-data-header/data.js";
+import footerData from "@openeuropa/bcl-data-footer/data.js";
+import groupLanding from "@openeuropa/bcl-group-landing/group-landing.html.twig";
+import demoData from "@openeuropa/bcl-group-landing/data";
+import defaultSprite from "@openeuropa/bcl-bootstrap/bootstrap-icons.svg";
+
+export default {
+  title: "Compositions/Group/Landing",
+  decorators: [withDesign],
+  parameters: {
+    layout: "fullscreen",
+    design: [
+      {
+        name: "Mockup - Group",
+        type: "figma",
+        url: "https://www.figma.com/file/NQlGvTiTXZYN8TwY2Ur5EI/BCL-Features?node-id=482%3A2559",
+      },
+    ],
+    controls: {
+      disable: true,
+    },
+  },
+};
+
+const correctPaths = (data) => {
+  data.data.header = headerData;
+  data.data.footer = footerData;
+  data.data.header.head.navigation.items.forEach((item) => {
+    if (item.icon) {
+      item.icon.path = defaultSprite;
+    }
+  });
+  data.data.header.navbar.form.submit.icon.path = defaultSprite;
+  data.data.header.breadcrumbs.icons_path = defaultSprite;
+  data.data.footer.rows.forEach((row) => {
+    row.cols.forEach((col) => {
+      if (col.items) {
+        col.items.forEach((item) => {
+          if (item.type == "links") {
+            item.links.forEach((link) => {
+              if (link.icon) {
+                link.icon.path = defaultSprite;
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+  data.data.recent.block_data.activities.forEach((activity) => {
+    activity.meta.forEach((meta) => {
+      meta.icon.path = defaultSprite;
+    });
+  });
+  data.data.banner_description.metas.forEach((meta) => {
+    meta.icon.path = defaultSprite;
+  });
+  data.data.banner_description.collapse_trigger.icon.path = defaultSprite;
+  data.data.contributions.listing.link.icon.path = defaultSprite;
+  data.data.members.more_link.icon.path = defaultSprite;
+  return data;
+};
+
+export const Default = () => groupLanding(correctPaths(demoData));
