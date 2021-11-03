9bd27c475bda223be20b87c6b22d63e1b955913f
planctus <davidepuntoferrante@gmail.com>
2021-11-03 10:49:19 +0200

    feat(fact-figures): Extending test coverage - front-3256

--- src/compositions/bcl-fact-figures/fact-figures.test.js
+++ src/compositions/bcl-fact-figures/fact-figures.test.js
@@ -6,9 +6,25 @@ const template = "@oe-bcl/bcl-fact-figures/fact-figures.html.twig";
 const render = (params) => renderTwigFileAsNode(template, params);
 
 describe("OE - Fact and figures", () => {
-  test(`renders correctly`, () => {
+  test(`renders correctly in 3 columns`, () => {
     expect.assertions(1);
 
     return expect(render(demoData)).resolves.toMatchSnapshot();
+  });
+
+  test(`renders correctly in 2 columns`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ ...demoData, responsive_columns: 2 })
+    ).resolves.toMatchSnapshot();
+  });
+
+  test(`renders correctly in 1 column`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ ...demoData, responsive_columns: 1 })
+    ).resolves.toMatchSnapshot();
   });
 });
