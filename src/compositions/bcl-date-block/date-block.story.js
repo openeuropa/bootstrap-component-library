5c1ff28ea0d57894a7d8132df2238a6c0b928a6a
planctus <davidepuntoferrante@gmail.com>
2021-11-09 16:23:35 +0200

    feat(date-block): Adding composition - FRONT-3320 (#124)

---
+++ src/compositions/bcl-date-block/date-block.story.js
@@ -0,0 +1,62 @@
+import dateBlock from "@openeuropa/bcl-date-block/date-block.html.twig";
+import demoData from "@openeuropa/bcl-date-block/data/data";
+
+const getArgs = (data) => {
+  return {
+    day: data.day,
+    month: data.month,
+    year: data.year,
+    date_time: data.date_time,
+  };
+};
+
+const getArgTypes = () => {
+  return {
+    day: {
+      type: { name: "string" },
+      description: "Day",
+      table: {
+        type: { summary: "integer" },
+        defaultValue: { summary: "" },
+      },
+    },
+    month: {
+      type: { name: "string" },
+      description: "Month",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "" },
+      },
+    },
+    year: {
+      type: { name: "string" },
+      description: "Year",
+      table: {
+        type: { summary: "integer" },
+        defaultValue: { summary: "" },
+      },
+    },
+    date_time: {
+      name: "datetime attribute",
+      type: { name: "string" },
+      description: "Valid date format",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "" },
+      },
+    },
+  };
+};
+
+const applyArgs = (data, args) => {
+  return Object.assign(data, args);
+};
+
+export default {
+  title: "Compositions/Date block",
+};
+
+export const Standalone = (args) => dateBlock(applyArgs(demoData));
+
+Standalone.args = getArgs(demoData);
+Standalone.argTypes = getArgTypes();
