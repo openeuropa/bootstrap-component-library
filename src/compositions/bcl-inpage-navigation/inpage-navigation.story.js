24ec3249b5ee4c8282732f0d83093146315c6cf3
Alexis Gaillard <a@bypopcorn.com>
2021-11-11 16:51:28 +0100

    feat(compositions): Add inpage navigation - FRONT-3138 (#130)

---
+++ src/compositions/bcl-inpage-navigation/inpage-navigation.story.js
@@ -0,0 +1,46 @@
+import dataDefault from "@openeuropa/bcl-inpage-navigation/dataDefault.js";
+import inpage from "@openeuropa/bcl-inpage-navigation/inpage-navigation.html.twig";
+
+const initScrollspy = (story) => {
+  const demo = story();
+  return `
+    <script>
+      if (
+        document.getElementById("bcl-inpage-navigation") &&
+        typeof bootstrap !== "undefined"
+      ) {
+        document.body.setAttribute("data-bs-spy", "scroll");
+        document.body.setAttribute("data-bs-target", "#bcl-inpage-navigation");
+        var scrollspyBody = bootstrap.ScrollSpy.getInstance(document.body);
+        if (scrollspyBody) {
+          scrollspyBody.dispose();
+        }
+        var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+          target: "#bcl-inpage-navigation",
+        });
+      }
+    </script>
+  ${demo}`;
+};
+
+export default {
+  title: "Compositions/Inpage navigation",
+  decorators: [initScrollspy],
+  parameters: {
+    controls: { disable: true },
+    design: [
+      {
+        name: "Mockup",
+        type: "figma",
+        url: "https://www.figma.com/file/7aJedLkk8hiDoD3RcxTnQi/BCL-Starter-kit?node-id=4195%3A41188",
+      },
+      {
+        name: "Bootstrap docs",
+        type: "iframe",
+        url: "https://getbootstrap.com/docs/5.0/components/scrollspy/",
+      },
+    ],
+  },
+};
+
+export const Default = () => inpage(dataDefault);
