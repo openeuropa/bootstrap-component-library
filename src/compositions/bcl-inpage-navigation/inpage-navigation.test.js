086d40d36823063a23ca3f8fab013db74b9dffde
Alexis Gaillard <a@bypopcorn.com>
2021-11-11 13:16:59 +0100

    Add inpage navigation

---
+++ src/compositions/bcl-inpage-navigation/inpage-navigation.test.js
@@ -0,0 +1,14 @@
+import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
+
+import dataDefault from "@openeuropa/bcl-inpage-navigation/dataDefault.js";
+
+const template = "@oe-bcl/bcl-inpage-navigation/inpage-navigation.html.twig";
+const render = (params) => renderTwigFileAsNode(template, params);
+
+describe("OE - Inpage navigation", () => {
+  test("default renders correctly", () => {
+    expect.assertions(1);
+
+    return expect(render(dataDefault)).resolves.toMatchSnapshot();
+  });
+});
