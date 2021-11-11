b6da25adf4dcb58da8f0faa3d02cfe4da74a5cf4
Alexis Gaillard <a@bypopcorn.com>
2021-11-11 16:44:22 +0100

    Set the inpage and timeline packages public

--- src/compositions/bcl-inpage-navigation/inpage-navigation.test.js
+++ src/compositions/bcl-inpage-navigation/inpage-navigation.test.js
@@ -1,5 +1,4 @@
 import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
-
 import dataDefault from "@openeuropa/bcl-inpage-navigation/dataDefault.js";
 
 const template = "@oe-bcl/bcl-inpage-navigation/inpage-navigation.html.twig";
