6ecb18d39e8e558feebe38a809053fe68c15ab6e
Alexis Gaillard <a@bypopcorn.com>
2021-12-21 10:24:16 +0100

    Add w-100 class to the image

--- src/compositions/bcl-featured-media/dataImage.js
+++ src/compositions/bcl-featured-media/dataImage.js
@@ -2,5 +2,5 @@ module.exports = {
   content: "Media description text goes here.",
   content_classes: "bg-lighter p-3",
   image:
-    "<img class='img-fluid' src='https://picsum.photos/1000/400?random=1'>",
+    "<img class='img-fluid w-100' src='https://picsum.photos/1000/400?random=1'>",
 };
