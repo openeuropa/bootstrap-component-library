cbe74548e92f5a022dcea6fd08e50e23c22b63ec
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-02-03 14:58:36 +0200

    Improved demo

--- src/compositions/bcl-multilingual/multilingual.story.js
+++ src/compositions/bcl-multilingual/multilingual.story.js
@@ -2,8 +2,13 @@ import { withDesign } from "storybook-addon-designs";
 import MultilingualDataEU from "@openeuropa/bcl-multilingual/data/data--eu.js";
 import MultilingualDataEC from "@openeuropa/bcl-multilingual/data/data--ec.js";
 import MultilingualDataNeutral from "@openeuropa/bcl-multilingual/data/data--neutral.js";
-import header from "@openeuropa/bcl-header/header.html.twig";
+import content from "@openeuropa/bcl-news/data/data_content.js";
+import banner from "@openeuropa/bcl-content-banner/data/data.js";
+import contentType from "@openeuropa/bcl-base-templates/content-type.html.twig";
+import footer from "@openeuropa/bcl-data-footer/data";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
+const feedback = `<div class="bg-lighter py-4 mt-md-4-5 mt-4 text-center">Feedback module here</div>`;
+const share = `<div class="bg-gray-200 py-4 text-center">Share module here</div>`;
 
 const openModal = (story) => {
   const demo = story();
@@ -18,6 +23,34 @@ const openModal = (story) => {
       languageModal.show()
     </script>
     `;
+};
+
+const baseData = {
+  content_type: "multilingual",
+  page_title: "Demo page",
+  footer: footer,
+  with_banner: true,
+  with_header: true,
+  with_footer: true,
+  banner: banner,
+  content: content,
+  feedback: feedback,
+  share: share,
+};
+
+const neutralData = {
+  ...baseData,
+  header: MultilingualDataNeutral,
+};
+
+const ECData = {
+  ...baseData,
+  header: MultilingualDataEC,
+};
+
+const EUData = {
+  ...baseData,
+  header: MultilingualDataEU,
 };
 
 export default {
@@ -35,12 +68,11 @@ export default {
   },
 };
 
-export const MultilingualNeutral = () =>
-  header(correctPaths(MultilingualDataNeutral));
+export const MultilingualNeutral = () => contentType(correctPaths(neutralData));
 MultilingualNeutral.storyName = "Neutral";
 
-export const MultilingualEC = () => header(correctPaths(MultilingualDataEC));
+export const MultilingualEC = () => contentType(correctPaths(ECData));
 MultilingualEC.storyName = "EC";
 
-export const MultilingualEU = () => header(correctPaths(MultilingualDataEU));
+export const MultilingualEU = () => contentType(correctPaths(EUData));
 MultilingualEU.storyName = "EU";
