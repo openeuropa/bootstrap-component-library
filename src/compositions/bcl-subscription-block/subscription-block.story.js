e047cc1f0bf9814975d9d1812ede75d77b979a6c
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-02-10 17:01:00 +0200

    Improvements

--- src/compositions/bcl-subscription-block/subscription-block.story.js
+++ src/compositions/bcl-subscription-block/subscription-block.story.js
@@ -1,11 +1,11 @@
 import { withDesign } from "storybook-addon-designs";
-import { correctPaths, initScrollspy } from "@openeuropa/bcl-story-utils";
+import { correctPaths } from "@openeuropa/bcl-story-utils";
 import subscriptionBlock from "@openeuropa/bcl-subscription-block/subscription-block.html.twig";
 import demoData from "@openeuropa/bcl-subscription-block/data/data.js";
 
 export default {
   title: "Compositions/Subscription Block",
-  decorators: [withDesign, initScrollspy],
+  decorators: [withDesign],
   parameters: {
     layout: "fullscreen",
     controls: { disable: true },
