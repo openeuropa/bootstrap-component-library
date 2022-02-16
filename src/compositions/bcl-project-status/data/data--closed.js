8aa70db9e2538e4ed6321de5ad1eb13437699a69
Alexis Gaillard <a@bypopcorn.com>
2022-02-16 14:41:12 +0100

    feat(project): Add circular progress bar for project status - OEL-1137 (#268)
    * feat(project): Add circular progress bar for project status - OEL-1137
    
    * Review color term label alignment and spacing in description-list
    
    * Update chart percentage according to budget
    
    * Cleanup scss and add it to sub-themes
    
    * Add story and review parameters naming
    
    * Pr feedback
    
    * Remove unnecessary story name parameter
    
    Co-authored-by: planctus <davidepuntoferrante@gmail.com>

--- src/compositions/bcl-project-status/data/data--closed.js
+++ src/compositions/bcl-project-status/data/data--closed.js
@@ -11,4 +11,25 @@ module.exports = {
   end_date: "19/12/2027",
   end_label: "End",
   progress: 100,
+  corporate_contribution: 70,
+  chart_legend: {
+    variant: "horizontal",
+    items: [
+      {
+        term: {
+          label: "Total budget",
+          color: "bg-gray-400",
+        },
+        definition: "EUR 16.314.672,00",
+      },
+      {
+        term: {
+          label: "EC contribution",
+          color: "bg-primary",
+        },
+        definition: "EUR 9.994.672,00",
+      },
+    ],
+    attributes: new drupalAttribute().addClass("mb-0"),
+  },
 };
