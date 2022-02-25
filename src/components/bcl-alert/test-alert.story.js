db3e8da0e309272d5c6e30caa815a3ef1718af5b
planctus <davidepuntoferrante@gmail.com>
2022-02-25 16:18:55 +0200

    chore(chromatic): recent activities

--- src/components/bcl-alert/test-alert.story.js
+++ src/components/bcl-alert/test-alert.story.js
@@ -1,5 +1,6 @@
 import demoData from "@openeuropa/bcl-data-alert/data.js";
 import alert from "@openeuropa/bcl-alert/alert.html.twig";
+import defaultSprite from "@openeuropa/bcl-bootstrap/bootstrap-icons.svg";
 
 export default {
   title: "Components/Alert",
@@ -9,6 +10,7 @@ export const WithoutIcon = () => alert({ ...demoData, icon_path: "" });
 
 WithoutIcon.storyName = "Without icon";
 
-export const WithoutHeading = () => alert({ ...demoData, heading: "" });
+export const WithoutHeading = () =>
+  alert({ ...demoData, icon_path: defaultSprite, heading: "" });
 
 WithoutHeading.storyName = "Without heading";
--- src/compositions/bcl-recent-activities/recent-activities.story.js
+++ src/compositions/bcl-recent-activities/recent-activities.story.js
@@ -1,6 +1,13 @@
 import demoData from "@openeuropa/bcl-recent-activities/data.js";
 import recentActivities from "@openeuropa/bcl-recent-activities/recent-activities.html.twig";
+import isChromatic from "chromatic/isChromatic";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
+
+if (isChromatic()) {
+  demoData.activities.forEach((item, i) => {
+    item.image = `<img class='rounded-circle chromatic-ignore me-3-5' alt='recent-image-${i}' src='https://picsum.photos/50/50?random=${i}'>`;
+  });
+}
 
 export default {
   title: "Compositions/Recent Activities",
