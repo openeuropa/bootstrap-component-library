9489350de8e6fbda43098576988b5adb85c1d801
planctus <davidepuntoferrante@gmail.com>
2022-03-02 10:57:24 +0200

    chore(chromatic): Adding script for running a build patch

---
+++ scripts/build-patch.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const { spawn } = require('child_process');
+const theme = process.argv.slice(2)[0] || 'default';
+const branch = process.argv.slice(2)[1] || 'master';
+
+const args = [
+	'chromatic',
+	'--force-rebuild',
+	'--exit-once-uploaded',
+	'--project-token=f44b33745fa7',
+	`--build-script-name=build:chromatic:${theme}`,
+	`--patch-build=${branch}...development`,
+];
+
+const buildPatch = spawn('npx', args, { stdio: "inherit", shell: true });
+
+buildPatch.on('error', (err) => console.log(`${name}: ${err.message}`));
+buildPatch.on('exit', () => {
+	console.log('Build patch process completed')
+});
