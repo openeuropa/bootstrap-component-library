4c977b9a3928c097f69731881a7e23286335165e
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-03-02 13:32:09 +0200

    Project details update

---
+++ src/compositions/bcl-project/data/data_status.js
@@ -0,0 +1,10 @@
+module.exports = {
+  main_title: "Overview",
+  main_title_id: "overview",
+  label: "Status",
+  start_date: "19/12/2020",
+  start_label: "Start",
+  end_date: "19/12/2027",
+  end_label: "End",
+  progress: 25,
+};
