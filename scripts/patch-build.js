4c4c71cb96781a05d24b6932985d211e33594140
planctus <davidepuntoferrante@gmail.com>
2022-03-04 14:54:05 +0200

    chore(workflow): Adding script to automatize the first steps of the development

--- scripts/patch-build.js
+++ scripts/patch-build.js
@@ -1,22 +1,24 @@
 #!/usr/bin/node
 
-const { spawn } = require('child_process');
-const theme = process.argv.slice(2)[0] || 'default';
-const branch = process.argv.slice(2)[1] || 'master';
-const token = process.argv.slice(2)[2] || '';
+const { spawn } = require("child_process");
+const theme = process.argv.slice(2)[0] || "default";
+const branch = process.argv.slice(2)[1] || "master";
+const token = process.argv.slice(2)[2] || "";
 
 const args = [
-	'chromatic',
-	'--ci',
-	'--force-rebuild',
-	`--project-token=${token}`,
-	`--build-script-name=build:chromatic:${theme}`,
-	`--patch-build ${branch}...development`,
+  "chromatic",
+  "--ci",
+  "--force-rebuild",
+  `--project-token=${token}`,
+  `--build-script-name=build:chromatic:${theme}`,
+  `--patch-build ${branch}...development`,
 ];
 
-const buildPatch = spawn('yarn', args, { stdio: "inherit" });
+const buildPatch = spawn("yarn", args, { stdio: "inherit" });
 
-buildPatch.on('error', (err) => console.log(`${name}: ${err.message}`));
-buildPatch.on('exit', () => {
-	console.log(`Build patch process ended for the ${theme} styleguide on the ${branch} branch`)
+buildPatch.on("error", (err) => console.log(err.message));
+buildPatch.on("exit", () => {
+  console.log(
+    `Build patch process ended for the ${theme} styleguide on the ${branch} branch`
+  );
 });
