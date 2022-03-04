bacc814b738769d46290a5929a65538b7b8b5dc0
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-03-04 17:55:04 +0200

    PR feedback

--- src/compositions/bcl-project/data/ucpkn/data_status.js
+++ src/compositions/bcl-project/data/ucpkn/data_status.js
@@ -1,6 +1,6 @@
 module.exports = {
-  main_title: "Overview",
-  main_title_id: "overview",
+  title: "Overview",
+  title_id: "overview",
   label: "Status",
   start_date: "19/12/2020",
   start_label: "Start",
