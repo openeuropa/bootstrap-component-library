106d4f5679a995b470cadb9bd1b9845e9f2afef4
tibi2303 <36895476+tibi2303@users.noreply.github.com>
2022-03-08 13:29:26 +0200

    feat(language-switcher): Added language switcher - OEL-1243 (#283)

---
+++ src/compositions/bcl-language-switcher/language-switcher.test.js
@@ -0,0 +1,14 @@
+import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
+
+import demoData from "@openeuropa/bcl-language-switcher/data/data.js";
+
+const template = "@oe-bcl/bcl-language-switcher/language-switcher.html.twig";
+const render = (params) => renderTwigFileAsNode(template, params);
+
+describe("OE - Language switcher", () => {
+  test("renders correctly", () => {
+    expect.assertions(1);
+
+    return expect(render(demoData)).resolves.toMatchSnapshot();
+  });
+});
