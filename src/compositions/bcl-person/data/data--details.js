a3b0fad7dfcd48b16003b0a4c9477dc156a86ff2
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-03-09 19:34:04 +0200

    Added person feature

---
+++ src/compositions/bcl-person/data/data--details.js
@@ -0,0 +1,135 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  banner: {
+    title: {
+      content: "Stefan Mayer",
+      tag: "h2",
+      attributes: new drupalAttribute().addClass("fw-bold"),
+    },
+    image: {
+      path: "https://picsum.photos/200/200?random=6",
+      alt: "alt img",
+      position: "top",
+    },
+    content: `<div class="mb-4 mt-4">
+      <span class="text-muted me-3">DG Test</span><span class="text-muted">Director</span>
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ultricies massa, a dapibus libero porta nec. Sed facilisis dictum vestibulum. Fusce commodo hendrerit diam, pretium tempus leo varius sit amet. Etiam interdum, orci at sagittis luctus, lorem libero tempus mauris, a fermentum libero orci semper lacus. Duis tristique fringilla magna, eu egestas dolor molestie non.</p>`,
+  },
+  social_media: {
+    main_title: "Social media",
+    links: [
+      {
+        label: "Linkedin",
+        icon_position: "before",
+        standalone: true,
+        path: "/example.html",
+        icon: {
+          name: "linkedin",
+          path: "/icons.svg",
+        },
+        attributes: new drupalAttribute().addClass([
+          "d-block",
+          "d-md-inline-block",
+          "mb-3",
+          "mb-md-0",
+          "me-md-3",
+        ]),
+      },
+      {
+        label: "Twitter",
+        icon_position: "before",
+        standalone: true,
+        path: "/example.html",
+        icon: {
+          name: "twitter",
+          path: "/icons.svg",
+        },
+        attributes: new drupalAttribute().addClass([
+          "d-block",
+          "d-md-inline-block",
+          "mb-3",
+          "mb-md-0",
+          "mx-md-3",
+        ]),
+      },
+      {
+        label: "Facebook",
+        icon_position: "before",
+        standalone: true,
+        path: "/example.html",
+        icon: {
+          name: "facebook",
+          path: "/icons.svg",
+        },
+        attributes: new drupalAttribute().addClass([
+          "d-block",
+          "d-md-inline-block",
+          "mb-3",
+          "mb-md-0",
+          "mx-md-3",
+        ]),
+      },
+      {
+        label: "Youtube",
+        icon_position: "before",
+        standalone: true,
+        path: "/example.html",
+        icon: {
+          name: "youtube",
+          path: "/icons.svg",
+        },
+        attributes: new drupalAttribute().addClass([
+          "d-block",
+          "d-md-inline-block",
+          "mb-3",
+          "mb-md-0",
+          "mx-md-3",
+        ]),
+      },
+    ],
+  },
+  information: {
+    main_title: "Additional information",
+    content: `
+    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae velit facilisis, ornare sem efficitur, euismod diam. Curabitur quis odio in nisl scelerisque rhoncus nec id nulla. Mauris ultrices a augue in finibus.</p>
+    <ul>
+      <li>Etiam sagittis dictum turpis, eu lacinia tellus pharetra ac</li>
+      <li>Sed auctor purus at tellus cursus dignissim. Sed sit amet ligula nisl</li>
+    </ul>
+    `,
+    files: [
+      {
+        item_title: "File title",
+        language: "English",
+        meta: "(16.2 MB - PDF)",
+        icon_path: "/icons.svg",
+        download: {
+          label: "Download",
+          url: "/example.html",
+        },
+        icon: {
+          name: "file-pdf-fill",
+          size: "xl",
+        },
+        attributes: new drupalAttribute().addClass(["mb-3"]),
+      },
+      {
+        item_title: "File title",
+        language: "English",
+        meta: "(16.2 MB - PDF)",
+        icon_path: "/icons.svg",
+        download: {
+          label: "Download",
+          url: "/example.html",
+        },
+        icon: {
+          name: "file-pdf-fill",
+          size: "xl",
+        },
+        attributes: new drupalAttribute().addClass(["mb-3"]),
+      },
+    ],
+  },
+};
---
+++ src/compositions/bcl-person/data/data--listing.js
@@ -0,0 +1,201 @@
+const drupalAttribute = require("drupal-attribute");
+
+module.exports = {
+  sidebar: {
+    id: "bcl-offcanvas",
+    attributes: new drupalAttribute().addClass("bcl-offcanvas offcanvas-start"),
+    extra_classes_close: "d-lg-none",
+    extra_classes_header: "p-lg-0",
+    with_body_scroll: true,
+    title: {
+      label: "Filter options",
+      heading: 4,
+      extra_classes: "mb-lg-4",
+    },
+    search_form: {
+      attributes: new drupalAttribute()
+        .setAttribute("novalidate", true)
+        .setAttribute("onsubmit", "return false;"),
+      items: [
+        [
+          {
+            classes: "mb-3",
+            label: "Name",
+            input_type: "text",
+            placeholder: "Placeholder",
+          },
+        ],
+      ],
+      submit: {
+        multiple: true,
+        wrapper: "mt-4",
+        items: [
+          {
+            label: "Refine",
+            type: "submit",
+            variant: "primary",
+            attributes: new drupalAttribute().addClass("me-2"),
+          },
+          {
+            label: "Clear",
+            type: "submit",
+            variant: "light",
+          },
+        ],
+      },
+    },
+  },
+  listing: {
+    variant: "default-1-col",
+    remove_horizontal_spacer: true,
+    items: [
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content:
+            "<a href='/example.html' class='standalone'>Stefan Mayer</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=1",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content:
+            "<a href='/example.html' class='standalone'>Adjur Tichar</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=2",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content: "<a href='/example.html' class='standalone'>Szép Mara</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=3",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content:
+            "<a href='/example.html' class='standalone'>Daniel Cardoso Rocha</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=4",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content:
+            "<a href='/example.html' class='standalone'>Aurelia Panicucci</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=5",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+      {
+        extra_classes_header: "bg-transparent pt-0 pb-0 border-0",
+        title: {
+          content:
+            "<a href='/example.html' class='standalone'>Felicyta Kowalska</a>",
+        },
+        content:
+          "<span class='text-muted me-4-5'>Organisation</span><span class='text-muted me-2'>Position</span>",
+        image: {
+          path: "https://picsum.photos/160?random=6",
+          alt: "alt img",
+          position: "top",
+        },
+      },
+    ],
+  },
+  pagination: {
+    attributes: new drupalAttribute().addClass(["mt-4-5", "mb-5"]),
+    alignment: "center",
+    prev: {
+      label: "Previous",
+      path: "#",
+    },
+    next: {
+      label: "Next",
+      path: "#",
+    },
+    items: [
+      {
+        label: "1",
+        path: "#",
+        active: true,
+      },
+      {
+        label: "2",
+        path: "#",
+      },
+      {
+        label: "3",
+        path: "#",
+      },
+      {
+        label: "4",
+        path: "#",
+      },
+      {
+        label: "5",
+        path: "#",
+      },
+    ],
+  },
+  filter_button: {
+    label: "Filter options",
+    type: "button",
+    size: "lg",
+    variant: "light",
+    attributes: new drupalAttribute()
+      .addClass(["d-lg-none", "w-100", "mb-4"])
+      .setAttribute("data-bs-toggle", "offcanvas")
+      .setAttribute("data-bs-target", "#bcl-offcanvas")
+      .setAttribute("aria-controls", "bcl-offcanvas"),
+    icon_position: "before",
+    icon: {
+      name: "filter",
+      path: "/icons.svg",
+      attributes: new drupalAttribute().addClass(["me-2"]),
+    },
+  },
+  sort_select: {
+    label: "Sort&nbsp;by",
+    attributes: new drupalAttribute().addClass("mb-4 mb-md-0 ms-md-4"),
+    aria_label: "sort-select",
+    options: [
+      { value: 1, label: "a select option" },
+      { value: 2, label: "another select option" },
+      { value: 3, label: "another option" },
+      { value: 4, label: "last option" },
+      { label: "A-Z", selected: true },
+    ],
+  },
+};
