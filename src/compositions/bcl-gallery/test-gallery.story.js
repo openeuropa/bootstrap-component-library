0cf88c3918efbfced4feafdfec6ad1e84a88531b
Alexis Gaillard <a@bypopcorn.com>
2022-03-16 15:33:10 +0100

    Update chromatic test file

--- src/compositions/bcl-gallery/test-gallery.story.js
+++ src/compositions/bcl-gallery/test-gallery.story.js
@@ -1,4 +1,3 @@
-import { withDesign } from "storybook-addon-designs";
 import { getByRole, userEvent } from "@storybook/testing-library";
 import dataDefault from "./data/dataDefault.js";
 import gallery from "@openeuropa/bcl-gallery/gallery.html.twig";
@@ -9,16 +8,6 @@ const chromatic = process.env.STORYBOOK_ENV;
 
 export default {
   title: "Paragraphs/Gallery",
-  decorators: [withDesign],
-  parameters: {
-    design: [
-      {
-        name: "Mockup",
-        type: "figma",
-        url: "https://www.figma.com/file/7aJedLkk8hiDoD3RcxTnQi/BCL-Starter-kit---Components?node-id=8654%3A83333",
-      },
-    ],
-  },
 };
 
 export const Default = () => gallery(correctPaths(dataDefault));
