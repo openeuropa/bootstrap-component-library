b58369c8c1c25064fc4339049b3d442acd27a67f
Alexis Gaillard <a@bypopcorn.com>
2022-03-16 15:32:07 +0100

    Add dedicated chromatic test file

--- src/compositions/bcl-gallery/test-gallery.story.js
+++ src/compositions/bcl-gallery/test-gallery.story.js
@@ -1,7 +1,11 @@
 import { withDesign } from "storybook-addon-designs";
+import { getByRole, userEvent } from "@storybook/testing-library";
 import dataDefault from "./data/dataDefault.js";
 import gallery from "@openeuropa/bcl-gallery/gallery.html.twig";
+import isChromatic from "chromatic/isChromatic";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
+
+const chromatic = process.env.STORYBOOK_ENV;
 
 export default {
   title: "Paragraphs/Gallery",
@@ -18,3 +22,13 @@ export default {
 };
 
 export const Default = () => gallery(correctPaths(dataDefault));
+
+if (isChromatic() || chromatic) {
+  Default.play = async () => {
+    const thumbnail = document.querySelector(
+      ".bcl-gallery__grid:first-child li:nth-child(2) a"
+    );
+    const link = getByRole(thumbnail, "img");
+    await userEvent.click(link);
+  };
+}
