69ce26a7f42879810e11deea66a7f277ecba3c69
tibi2303 <36895476+tibi2303@users.noreply.github.com>
2022-03-17 13:38:32 +0200

    feat(content-banner): Content banner update - OEL-1323 (#298)

--- src/compositions/bcl-content-banner/content-banner.story.js
+++ src/compositions/bcl-content-banner/content-banner.story.js
@@ -7,9 +7,10 @@ import contentBanner from "@openeuropa/bcl-content-banner/content-banner.html.tw
 
 const button = { ...dataDefault.button };
 
-const getArgs = (data) => {
+const getArgs = () => {
   return {
     background: "gray",
+    image_size: "md",
     action_button: false,
   };
 };
@@ -26,12 +27,22 @@ const getArgTypes = () => {
       },
     },
     action_button: {
-      name: "Action button",
+      name: "action button",
       type: { name: "boolean" },
       description: "Toggle button",
       table: {
         type: { summary: "object" },
         defaultValue: { summary: "{}" },
+      },
+    },
+    image_size: {
+      name: "image size",
+      type: { name: "select" },
+      options: ["sm", "md", "lg"],
+      description: "Size of the image",
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "md" },
       },
     },
   };
@@ -53,6 +64,7 @@ const applyArgs = (data, args) => {
   }
 
   data.background = args.background;
+  data.image_size = args.image_size;
   correctPaths(data);
 
   return data;
@@ -60,9 +72,19 @@ const applyArgs = (data, args) => {
 
 export default {
   title: "Compositions/Content banner",
+  parameters: {
+    layout: "fullscreen",
+    design: [
+      {
+        name: "Mockup - Content banner",
+        type: "figma",
+        url: "https://www.figma.com/file/7aJedLkk8hiDoD3RcxTnQi/BCL-Starter-kit---Components?node-id=9664%3A107055",
+      },
+    ],
+    badges: ["stable"],
+  },
 };
 
 export const Default = (args) => contentBanner(applyArgs(dataDefault, args));
-
 Default.args = getArgs();
 Default.argTypes = getArgTypes();
