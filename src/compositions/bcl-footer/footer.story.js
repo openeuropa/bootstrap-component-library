6744774215866a144092f9f499901ae0416bccc9
planctus <davidepuntoferrante@gmail.com>
2022-03-17 16:56:17 +0200

    feat(footer): Wrapping inline links in block elements - OEL-1327 (#301)
    * OEL-1327-2-#default
    [skip-actions]
    
    * feat(footer): Wrapping links - OEL-1327 [chromatic]
    
    * feat(chromatic): Adding ignore in person stories [chromatic]

--- src/compositions/bcl-footer/footer.story.js
+++ src/compositions/bcl-footer/footer.story.js
@@ -1,10 +1,14 @@
 import { withDesign } from "storybook-addon-designs";
+import isChromatic from "chromatic/isChromatic";
+import { userEvent, within } from "@storybook/testing-library";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
 
 import footer from "@openeuropa/bcl-footer/footer.html.twig";
 import footerData from "@openeuropa/bcl-data-footer/data";
 import footerDataEU from "@openeuropa/bcl-data-footer/data--eu";
 import footerDataEC from "@openeuropa/bcl-data-footer/data--ec";
+
+const chromatic = process.env.STORYBOOK_ENV;
 
 export default {
   title: "Compositions/Footer",
@@ -35,6 +39,13 @@ NeutralFooter.storyName = "Neutral footer";
 export const FooterEU = () => footer(correctPaths(footerDataEU));
 
 FooterEU.storyName = "Footer EU";
+if (isChromatic() || chromatic) {
+  FooterEU.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const link = canvas.getByText("Use other telephone options");
+    await userEvent.hover(link);
+  };
+}
 
 export const FooterEC = (args) => footer(correctPaths(footerDataEC));
 
