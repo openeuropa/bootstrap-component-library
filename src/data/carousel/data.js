fd1fe4c949b5c6c69f780529123cab8405d16612
Alexis Gaillard <a@bypopcorn.com>
2022-03-29 09:18:28 +0200

    feat(gallery): Added gallery paragraph - OEL-1312 (#295)

--- src/data/carousel/data.js
+++ src/data/carousel/data.js
@@ -7,8 +7,8 @@ module.exports = {
   items: [
     {
       classes: "item_extra_class",
-      caption: `<h5>First slide label</h5><p>Some representative placeholder content for the first slide.</p>`,
-      caption_classes: "d-none d-md-block",
+      caption: `<p>Some representative placeholder content for the first slide.</p>`,
+      caption_title: "First slide label",
       image: `<img
                 src="https://picsum.photos/800/400?random=1"
                 alt="First slide"
@@ -32,8 +32,8 @@ module.exports = {
     },
     {
       classes: "item_extra_class",
-      caption: `<h5>Third slide label</h5><p>Some representative placeholder content for the third slide.</p>`,
-      caption_classes: "d-none d-md-block",
+      caption_title: "Third slide label",
+      caption: `<p>Some representative placeholder content for the third slide.<`,
       image: `<img
                 src="https://picsum.photos/800/400?random=3"
                 class="d-block w-100"
@@ -41,7 +41,6 @@ module.exports = {
     },
     {
       classes: "item_extra_class",
-      caption_classes: "d-none d-md-block",
       image: `<img
                 src="https://picsum.photos/800/400?random=4"
                 alt="First slide"
