c5535cf3f41627c2986ca28eb0c14254e097a1fe
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-03-31 14:03:16 +0300

    Improvement [skip chromatic]

--- src/compositions/bcl-base-templates/data/layout.js
+++ src/compositions/bcl-base-templates/data/layout.js
@@ -1,6 +1,5 @@
-import header from "@openeuropa/bcl-data-header/data";
 import headerSimple from "@openeuropa/bcl-data-header/data--simple";
 import headerNoForm from "@openeuropa/bcl-data-header/data--no-form";
 import footer from "@openeuropa/bcl-data-footer/data";
 
-export { header, headerSimple, headerNoForm, footer };
+export { headerSimple, headerNoForm, footer };
