6362b23c050498fdbb0b2a8e790dfc43780b5de5
planctus <davidepuntoferrante@gmail.com>
2022-04-07 12:03:51 +0300

    feat(headings): Adding bcl-heading template - OEL-1420

---
+++ src/components/bcl-heading/heading.test.js
@@ -0,0 +1,37 @@
+import { merge, renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
+const template = "@oe-bcl/bcl-heading/heading.html.twig";
+const render = (params) => renderTwigFileAsNode(template, params);
+
+describe("OE - Heading", () => {
+  test(`renders correctly`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ title: 'A title created by the bcl heading template' })
+    ).resolves.toMatchSnapshot();
+  });
+
+  test(`renders correctly with custom tag`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ title: 'A title created by the bcl heading template', title_tag: 'h3' })
+    ).resolves.toMatchSnapshot();
+  });
+
+  test(`renders correctly with a link`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ link: { path:'/example', label: 'A title created by the bcl heading template' }})
+    ).resolves.toMatchSnapshot();
+  });
+
+  test(`renders correctly with a link and a title`, () => {
+    expect.assertions(1);
+
+    return expect(
+      render({ link: { path:'/example', title: 'A title created by the bcl heading template' }})
+    ).resolves.toMatchSnapshot();
+  });
+});
--- src/compositions/bcl-project/data/ucpkn/data--status.js
+++ src/compositions/bcl-project/data/ucpkn/data--status.js
@@ -1,6 +1,8 @@
+const drupalAttribute = require("drupal-attribute");
+
 module.exports = {
   title: "Overview",
-  title_id: "overview",
+  title_attributes: new drupalAttribute().setAttribute("id", "overview"),
   label: "Status",
   start_date: "19/12/2020",
   start_label: "Start",
