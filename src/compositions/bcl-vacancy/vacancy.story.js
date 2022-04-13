1e80841bd4f034ea614443add1c802fc8787547e
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-04-13 17:54:10 +0300

    Vacancy feature

---
+++ src/compositions/bcl-vacancy/vacancy.story.js
@@ -0,0 +1,113 @@
+import { withDesign } from "storybook-addon-designs";
+import isChromatic from "chromatic/isChromatic";
+import { initBadges, correctPaths } from "@openeuropa/bcl-story-utils";
+import drupalAttribute from "drupal-attribute";
+import {
+  headerSimple,
+  footer,
+} from "@openeuropa/bcl-base-templates/data/layout";
+import {
+  filterButton,
+  pagination,
+  sortSelect,
+} from "@openeuropa/bcl-base-templates/data/listing-page";
+import {
+  simpleBanner,
+  banner,
+} from "@openeuropa/bcl-base-templates/data/content-page";
+import listingPage from "@openeuropa/bcl-base-templates/listing-page.html.twig";
+
+import dataListing from "@openeuropa/bcl-vacancy/data/data--listing";
+import dataContent from "@openeuropa/bcl-vacancy/data/data";
+import dataSidebarDetails from "@openeuropa/bcl-vacancy/data/data--sidebar-details";
+import detailsPage from "@openeuropa/bcl-vacancy/vacancy.html.twig";
+
+if (isChromatic()) {
+  simpleBanner.image.classes = "chromatic-ignore";
+  banner.image.classes = "chromatic-ignore";
+}
+
+const sidebarOpen = {
+  ...dataSidebarDetails,
+  button: {
+    label: "Apply now",
+  },
+};
+
+const sidebarClosed = {
+  ...dataSidebarDetails,
+  button: {
+    label: "Vacancy closed",
+  },
+};
+
+const baseData = {
+  content_type: "listing",
+  page_title: "Projects",
+  header: headerSimple,
+  footer: {
+    ...footer,
+    attributes: new drupalAttribute().addClass("mt-4"),
+  },
+  with_header: true,
+  with_footer: true,
+  with_sidebar: true,
+};
+
+const demoDetails = {
+  ...baseData,
+  ...dataContent,
+  sidebar: sidebarOpen,
+  banner: { ...simpleBanner, background: "gray" },
+  social_block: true,
+};
+
+const demoDetailsClosed = {
+  ...baseData,
+  ...dataContent,
+  sidebar: sidebarClosed,
+  banner: { ...simpleBanner, background: "gray" },
+  social_block: true,
+};
+
+const demoListing = {
+  ...baseData,
+  ...dataListing,
+  banner: { ...banner, image_size: "sm", background: "white" },
+  pagination: pagination,
+  filter_button: filterButton,
+  sort_select: sortSelect,
+};
+
+export default {
+  title: "Features/Vacancy",
+  decorators: [withDesign],
+  parameters: {
+    layout: "fullscreen",
+    controls: {
+      disable: true,
+    },
+    badges: ["stable"],
+    badgesConfig: {
+      stable: {
+        title: "v1.1",
+      },
+    },
+    design: [
+      {
+        name: "Mockup",
+        type: "figma",
+        url: "https://www.figma.com/file/NQlGvTiTXZYN8TwY2Ur5EI/BCL-Features?node-id=10373%3A187235",
+      },
+    ],
+  },
+};
+
+export const Listing = () => listingPage(correctPaths(demoListing));
+Listing.decorators = [initBadges];
+
+export const Opened = () => detailsPage(correctPaths(demoDetails));
+Opened.storyName = "Details (opened)";
+
+export const Closed = () => detailsPage(correctPaths(demoDetailsClosed));
+Closed.storyName = "Details (closed)";
---
+++ src/compositions/bcl-vacancy/vacancy.test.js
@@ -0,0 +1,99 @@
+import { renderTwigFileAsNode } from "@openeuropa/bcl-test-utils";
+
+import {
+  headerSimple,
+  footer,
+} from "@openeuropa/bcl-base-templates/data/layout";
+import drupalAttribute from "drupal-attribute";
+import {
+  filterButton,
+  pagination,
+  sortSelect,
+} from "@openeuropa/bcl-base-templates/data/listing-page";
+import {
+  simpleBanner,
+  banner,
+} from "@openeuropa/bcl-base-templates/data/content-page";
+
+import dataListing from "@openeuropa/bcl-publication/data/data--listing";
+import dataContent from "@openeuropa/bcl-publication/data/data";
+import dataSidebarDetails from "@openeuropa/bcl-vacancy/data/data--sidebar-details";
+
+const listingTemplate = "@oe-bcl/bcl-base-templates/listing-page.html.twig";
+const detailsTemplate = "@oe-bcl/bcl-vacancy/vacancy.html.twig";
+
+const sidebarOpen = {
+  ...dataSidebarDetails,
+  button: {
+    label: "Apply now",
+  },
+};
+
+const sidebarClosed = {
+  ...dataSidebarDetails,
+  button: {
+    label: "Vacancy closed",
+    attributes: new drupalAttribute().setAttribute("disabled", "true"),
+  },
+};
+
+const baseData = {
+  content_type: "listing",
+  page_title: "Projects",
+  header: headerSimple,
+  footer: {
+    ...footer,
+    attributes: new drupalAttribute().addClass("mt-4"),
+  },
+  with_header: true,
+  with_footer: true,
+  with_sidebar: true,
+};
+
+const demoDetails = {
+  ...baseData,
+  ...dataContent,
+  sidebar: sidebarOpen,
+  banner: { ...simpleBanner, background: "gray" },
+  social_block: true,
+};
+
+const demoDetailsClosed = {
+  ...baseData,
+  ...dataContent,
+  sidebar: sidebarClosed,
+  banner: { ...simpleBanner, background: "gray" },
+  social_block: true,
+};
+
+const demoListing = {
+  ...baseData,
+  ...dataListing,
+  banner: { ...banner, image_size: "sm", background: "white" },
+  pagination: pagination,
+  filter_button: filterButton,
+  sort_select: sortSelect,
+};
+
+const renderListing = (params) =>
+  renderTwigFileAsNode(listingTemplate, params, true);
+
+const renderDetails = (params) =>
+  renderTwigFileAsNode(detailsTemplate, params, true);
+
+describe("OE - Vacancy", () => {
+  test("listing renders correctly", () => {
+    expect.assertions(1);
+    return expect(renderListing(demoListing)).resolves.toMatchSnapshot();
+  });
+
+  test("details (closed) renders correctly", () => {
+    expect.assertions(1);
+    return expect(renderDetails(demoDetails)).resolves.toMatchSnapshot();
+  });
+
+  test("details (closed) renders correctly", () => {
+    expect.assertions(1);
+    return expect(renderDetails(demoDetailsClosed)).resolves.toMatchSnapshot();
+  });
+});
