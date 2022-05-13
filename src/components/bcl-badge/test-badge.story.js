02e562e097c2f61a0df8f9e96b59848e41d940d6
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-05-13 11:38:46 +0300

    Added tests and interactions for hover and focus [skip chromatic]

--- src/components/bcl-badge/test-badge.story.js
+++ src/components/bcl-badge/test-badge.story.js
@@ -1,8 +1,13 @@
 import { initBadges } from "@openeuropa/bcl-story-utils";
 import withCode from "@openeuropa/storybook-addon-code";
+import isChromatic from "chromatic/isChromatic";
+import { within, userEvent } from "@storybook/testing-library";
+import drupalAttribute from "drupal-attribute";
 import demoData from "@openeuropa/bcl-data-badge/data.js";
 import badge from "@openeuropa/bcl-badge/badge.html.twig";
 import defaultSprite from "@openeuropa/bcl-theme-default/icons/bcl-default-icons.svg";
+
+const chromatic = process.env.STORYBOOK_ENV;
 
 demoData.icon_path = defaultSprite;
 
@@ -20,3 +25,18 @@ RoundedPill.storyName = "Rounded pill";
 export const Outline = () => badge({ ...demoData, outline: true });
 
 export const Link = () => badge({ ...demoData, url: true });
+
+export const HoverFocus = () =>
+  badge({ ...demoData, url: true, outline: true, dismissible: true });
+HoverFocus.storyName = "Hover and focus";
+if (isChromatic() || chromatic) {
+  HoverFocus.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const link = canvas.getByTitle("A simple badge!", {
+      selector: "a",
+    });
+
+    await userEvent.tab(link);
+  };
+}
