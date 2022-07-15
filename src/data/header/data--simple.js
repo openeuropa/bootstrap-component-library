165db2c285e803372a445c03649ed3044fa9c8e9
planctus <davidepuntoferrante@gmail.com>
2022-07-15 16:27:35 +0300

    feat(header): collapsible top navbar, data refactoring - OEL-1847

--- src/data/header/data--simple.js
+++ src/data/header/data--simple.js
@@ -1,13 +1,18 @@
 const drupalAttribute = require("drupal-attribute");
-const { getDummyText } = require("@openeuropa/bcl-data-utils");
+
+const languageList = require("@openeuropa/bcl-language-list/data/data-modal");
+const breadcrumbs = require("@openeuropa/bcl-data-breadcrumb/data");
+const navbar = require("@openeuropa/bcl-data-header/data--navbar");
+const modals = require("@openeuropa/bcl-data-header/data--modals");
 
 module.exports = {
   variant: "eu",
   head: {
     attributes: new drupalAttribute()
-      .addClass(["w-100", "shadow-sm"])
+      .addClass(["w-100", "shadow-sm", "bcl-header__top-navbar"])
       .setAttribute("aria-label", "Top Navigation"),
-    disable_collapse: true,
+    disable_collapse: false,
+    collapse_id: "topNavbarDropdown",
     brand: {
       link: "/example.html",
       logos: [
@@ -26,7 +31,7 @@ module.exports = {
     navigation: {
       items: [
         {
-          label: "<span class='d-block d-lg-inline-block'>English</span>",
+          label: "English",
           path: "/example.html",
           icon_position: "before",
           remove_icon_spacers: true,
@@ -34,15 +39,14 @@ module.exports = {
             name: "chat-left-dots-fill",
             size: "xs",
             path: "/icons.svg",
-            attributes: new drupalAttribute().addClass("me-lg-2-5"),
+            attributes: new drupalAttribute().addClass("me-2-5"),
           },
           attributes: new drupalAttribute()
             .setAttribute("data-bs-toggle", "modal")
-            .setAttribute("data-bs-target", "#languageModal")
-            .addClass("text-center"),
+            .setAttribute("data-bs-target", "#languageModal"),
         },
         {
-          label: "<span class='d-block d-lg-inline-block'>Log in</span>",
+          label: "Log in",
           path: "/example.html",
           icon_position: "before",
           remove_icon_spacers: true,
@@ -50,243 +54,20 @@ module.exports = {
             name: "person-fill",
             size: "xs",
             path: "/icons.svg",
-            attributes: new drupalAttribute().addClass("me-lg-2-5"),
+            attributes: new drupalAttribute().addClass("me-2-5"),
           },
           attributes: new drupalAttribute()
             .setAttribute("data-bs-toggle", "modal")
-            .setAttribute("data-bs-target", "#loginModal")
-            .addClass("text-center"),
+            .setAttribute("data-bs-target", "#loginModal"),
         },
       ],
     },
   },
-  navbar: {
-    color_set: "dark",
-    collapse_id: "navbarNavDropdown",
-    attributes: new drupalAttribute()
-      .addClass("bcl-header__navbar")
-      .setAttribute("aria-label", "main-navigation"),
-    form: {
-      attributes: new drupalAttribute().addClass(["d-flex", "mt-3", "mt-lg-0"]),
-      submit: {
-        variant: "light",
-        assistive_text: "search",
-        icon: {
-          name: "search",
-          size: "xs",
-          path: "/icons.svg",
-        },
-        attributes: new drupalAttribute().addClass([
-          "border-start-0",
-          "rounded-0",
-          "rounded-end",
-        ]),
-      },
-      items: [
-        [
-          {
-            type: "text",
-            required: true,
-            label: "Search",
-            hidden_label: true,
-            placeholder: "Search",
-            id: "inlineFormInputGroupSearch",
-            attributes: new drupalAttribute().addClass([
-              "border-start-0",
-              "rounded-0",
-              "rounded-start",
-            ]),
-          },
-        ],
-      ],
-    },
-    navigation: {
-      navbar: true,
-      attributes: new drupalAttribute().addClass("me-auto"),
-      items: [
-        {
-          label: "Home",
-          path: "/example.html",
-          active: true,
-          attributes: new drupalAttribute()
-            .setAttribute("aria-current", "page")
-            .setAttribute("aria-label", "homepage"),
-        },
-        {
-          label: "Item 01",
-          path: "/example.html",
-        },
-        {
-          label: "Item 02",
-          path: "/example.html",
-        },
-        {
-          id: "navbarDropdownMenuLink",
-          link: true,
-          dropdown: true,
-          trigger: {
-            label: "Dropdown link",
-            path: "/example.html",
-            attributes: new drupalAttribute().addClass("nav-link"),
-          },
-          items: [
-            {
-              label: "Action",
-              path: "/example.html",
-            },
-            {
-              label: "Another action",
-              path: "/example.html",
-            },
-            {
-              label: "Something else here",
-              path: "/example.html",
-            },
-          ],
-        },
-      ],
-    },
-  },
-  modals: [
-    {
-      id: "loginModal",
-      header: `<h5 class="modal-title" id="loginModalLabel">Log in</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`,
-      body: `<h5>Disclaimer 1</h5>
-        ${getDummyText(3, true)}
-        <h5>Disclaimer 2</h5>
-        ${getDummyText(3, true)}`,
-      footer: `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-         <button type="button" class="btn btn-primary">Access EU login</button>`,
-    },
-  ],
-  language_modal: {
-    id: "languageModal",
-    title: "Select your language",
-    icon_path: "/icons.svg",
-    close_label: "Close",
-    overlay: true,
-    items: [
-      {
-        label: "български",
-        lang: "bg",
-      },
-      {
-        label: "español",
-        lang: "es",
-      },
-      {
-        label: "čeština",
-        lang: "cs",
-      },
-      {
-        label: "dansk",
-        lang: "da",
-      },
-      {
-        label: "Deutsch",
-        lang: "de",
-      },
-      {
-        label: "eesti",
-        lang: "et",
-      },
-      {
-        label: "ελληνικά",
-        lang: "el",
-      },
-      {
-        label: "English",
-        active: true,
-        lang: "en",
-      },
-      {
-        label: "français",
-        lang: "fr",
-      },
-      {
-        label: "Gaeilge",
-        lang: "ga",
-      },
-      {
-        label: "hrvatski",
-        lang: "hr",
-      },
-      {
-        label: "italiano",
-        lang: "it",
-      },
-      {
-        label: "latviešu",
-        lang: "lv",
-      },
-      {
-        label: "lietuvių",
-        lang: "lt",
-      },
-      {
-        label: "magyar",
-        lang: "hu",
-      },
-      {
-        label: "Malti",
-        lang: "mt",
-      },
-      {
-        label: "Nederlands",
-        lang: "nl",
-      },
-      {
-        label: "polski",
-        lang: "pl",
-      },
-      {
-        label: "português",
-        lang: "pt",
-      },
-      {
-        label: "română",
-        lang: "ro",
-      },
-      {
-        label: "slovenčina",
-        lang: "sk",
-      },
-      {
-        label: "slovenščina",
-        lang: "sl",
-      },
-      {
-        label: "suomi",
-        lang: "fi",
-      },
-      {
-        label: "svenska",
-        lang: "sv",
-      },
-    ],
-  },
+  navbar,
+  modals,
+  language_modal: { ...languageList, variant: "eu" },
   breadcrumbs: {
+    ...breadcrumbs,
     attributes: new drupalAttribute().addClass(["mt-3"]),
-    links: [
-      {
-        label: "Home",
-        path: "/example.html",
-        attributes: new drupalAttribute().setAttribute(
-          "aria-label",
-          "homepage"
-        ),
-      },
-      {
-        label: "European Union",
-        path: "/example.html",
-      },
-      {
-        label: "About the European Union",
-        path: "/example.html",
-      },
-      { label: "News" },
-    ],
-    icon_path: "/icons.svg",
   },
 };
