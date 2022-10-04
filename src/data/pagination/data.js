869f357f2c2d0d198c3775cfa64394aeaaa303d1
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-10-04 14:53:16 +0300

    Removed dead element functionality [skip chromatic]

--- src/data/pagination/data.js
+++ src/data/pagination/data.js
@@ -36,16 +36,5 @@ module.exports = {
       label: "3",
       path: "/example.html",
     },
-    {
-      label: "...",
-    },
-    {
-      label: "4",
-      path: "/example.html",
-    },
-    {
-      label: "10",
-      path: "/example.html",
-    },
   ],
 };
