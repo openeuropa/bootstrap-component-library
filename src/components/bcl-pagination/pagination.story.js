0918e7c47630ddbf6e2cd781c1ef5ed42a032caf
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2022-10-06 16:39:31 +0300

    PR feedback [skip chromatic]

--- src/components/bcl-pagination/pagination.story.js
+++ src/components/bcl-pagination/pagination.story.js
@@ -2,7 +2,7 @@ import { withDesign } from "storybook-addon-designs";
 import withCode from "@openeuropa/storybook-addon-code";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
 
-import demoData from "@openeuropa/bcl-data-pagination/data";
+import demoData from "@openeuropa/bcl-data-pagination/data.js";
 import pagination from "@openeuropa/bcl-pagination/pagination.html.twig";
 
 const getArgs = () => ({
@@ -64,8 +64,8 @@ const getArgTypes = (data) => {
     icon_path: {
       name: "path to bcl sprite file (icon_path)",
       type: { name: "boolean" },
-      description:
-        "items like prev, next, first, last will be rendered as icons instead of html entities, it is also possible to pass icon objects along those items to use custom icons.",
+      description: `items like prev, next, first, last will be rendered as icons instead of html entities,
+        it is also possible to pass icon objects along those items to use custom icons.`,
       table: {
         type: { summary: "string" },
         defaultValue: { summary: "" },
@@ -96,21 +96,6 @@ const applyArgs = (data, args) => {
 
   if (args.alignment === "default") {
     args.alignment = "";
-  }
-
-  if (args.enable_prev_next_icon) {
-    data.prev.label = "";
-    data.next.label = "";
-  } else {
-    data.prev.label = "Previous";
-    data.next.label = "Next";
-  }
-  if (args.enable_first_last_icon) {
-    data.first.label = "";
-    data.last.label = "";
-  } else {
-    data.first.label = "First";
-    data.last.label = "Last";
   }
 
   if (args.icon_path) {
