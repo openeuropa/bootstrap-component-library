ac8e41121233f0c1844ea7f5b307f5f9b500ea99
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2025-04-08 20:15:48 +0300

    Mega menu desktop

--- src/compositions/bcl-mega-menu/mega-menu.story.js
+++ src/compositions/bcl-mega-menu/mega-menu.story.js
@@ -1,13 +1,10 @@
-import { withDesign } from "storybook-addon-designs";
 import { correctPaths } from "@openeuropa/bcl-story-utils";
-import withCode from "@openeuropa/storybook-addon-code";
 
 import demoData from "@openeuropa/bcl-mega-menu/data.js";
 import megaMenu from "@openeuropa/bcl-mega-menu/mega-menu.html.twig";
 
 export default {
   title: "Compositions/Mega Menu",
-  decorators: [withCode, withDesign],
 };
 
 const applyArgs = (data) => {
