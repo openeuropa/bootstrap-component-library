abf03f7d0962caef34cd47585bf8d24d8e9d995f
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2025-04-10 18:34:14 +0300

    feat(slim-select): Removed patch for slim-select 2 and applied in js directly.

--- src/themes/default/src/js/index.esm.js
+++ src/themes/default/src/js/index.esm.js
@@ -21,6 +21,7 @@ import Tooltip from "@openeuropa/bcl-bootstrap/js/src/tooltip";
 import Gallery from "@openeuropa/bcl-theme-default/src/js/gallery/gallery";
 import AccordionToggle from "@openeuropa/bcl-theme-default/src/js/accordion-toggle/accordion-toggle";
 import AccessibleToggle from "@openeuropa/bcl-theme-default/src/js/accessible-toggle/accessible-toggle";
+import SlimSelectWrapper from "@openeuropa/bcl-theme-default/src/js/slim-select-wrapper/slim-select-wrapper";
 
 export {
   Alert,
@@ -39,4 +40,5 @@ export {
   Gallery,
   AccessibleToggle,
   AccordionToggle,
+  SlimSelectWrapper,
 };
