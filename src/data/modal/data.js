71addc965cdfd1fece892f17e424a2119da3ba9f
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2025-10-20 13:14:32 +0300

    UCPKN-3880: Updated tests.

--- src/data/modal/data.js
+++ src/data/modal/data.js
@@ -1,14 +1,14 @@
 const { getDummyText } = require("@openeuropa/bcl-data-utils");
-const drupalAttribute = require("drupal-attribute");
+const { DrupalAttribute } = require("drupal-attribute");
 
 module.exports = {
   id: "exampleModal",
   header:
     '<h5 class="modal-title" id="exampleModalLabel">Modal title</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>',
-  header_attributes: new drupalAttribute().addClass("test-class"),
+  header_attributes: new DrupalAttribute().addClass("test-class"),
   body: getDummyText(7, true),
-  body_attributes: new drupalAttribute().addClass("test-class"),
+  body_attributes: new DrupalAttribute().addClass("test-class"),
   footer:
     '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button>',
-  footer_attributes: new drupalAttribute().addClass("test-class"),
+  footer_attributes: new DrupalAttribute().addClass("test-class"),
 };
--- src/data/toasts/data.js
+++ src/data/toasts/data.js
@@ -1,4 +1,4 @@
-const drupalAttribute = require("drupal-attribute");
+const { DrupalAttribute } = require("drupal-attribute");
 
 module.exports = {
   toasts: [
@@ -7,14 +7,14 @@ module.exports = {
       aria_live: "assertive",
       with_close: true,
       header: "Toast title",
-      header_attributes: new drupalAttribute().addClass("test-class"),
-      button_attributes: new drupalAttribute().addClass("test-class"),
+      header_attributes: new DrupalAttribute().addClass("test-class"),
+      button_attributes: new DrupalAttribute().addClass("test-class"),
       body: `Hello, world! This is a toast message.
             <div class="mt-2 pt-2 border-top">
               <button type="button" class="btn btn-primary btn-sm me-1">Take action</button>
               <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Close</button>
             </div>`,
-      body_attributes: new drupalAttribute().addClass("test-class"),
+      body_attributes: new DrupalAttribute().addClass("test-class"),
       custom_content: "<p>Toast custom content.</p>",
     },
   ],
