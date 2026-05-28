3c4d2991fce505a01814f59fb26d2384821880f5
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-05-28 17:37:20 +0300

    OEL-4774: Github check for generated files.

---
+++ scripts/check-generated-files.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+expected_files=(
+  "src/components/bcl-twig-templates/templates/bcl-accordion/accordion.html.twig"
+  "src/components/bcl-twig-templates/templates/bcl-card/card.html.twig"
+  "src/themes/default/templates/bcl-accordion/accordion.html.twig"
+  "src/themes/default/templates/bcl-card/card.html.twig"
+  "src/themes/joinup/templates/bcl-accordion/accordion.html.twig"
+  "src/themes/joinup/templates/bcl-card/card.html.twig"
+  "assets/index.html"
+  "assets/oe-bcl-default.min.css"
+  "assets/oe-bcl-default.bundle.min.js"
+  "assets/oe-bcl-joinup.min.css"
+  "assets/oe-bcl-joinup.bundle.min.js"
+  "assets/slimselect.min.js"
+  "assets/fonts/latin-400.css"
+  "bootstrap/icons/twitter.svg"
+  "bootstrap/js/src/alert.js"
+  "bootstrap/js/dist/alert.js"
+  "bootstrap/scss/bootstrap.scss"
+  "bootstrap/scss/_variables.scss"
+)
+
+unexpected_paths=(
+  "src/themes/default/templates/bcl-twig-templates"
+  "src/themes/joinup/templates/bcl-twig-templates"
+)
+
+failed=0
+
+for file in "${expected_files[@]}"; do
+  if [[ ! -s "$file" ]]; then
+    echo "::error file=$file::Expected generated file is missing or empty"
+    failed=1
+  fi
+done
+
+for path in "${unexpected_paths[@]}"; do
+  if [[ -e "$path" ]]; then
+    echo "::error file=$path::Generated templates were copied into a nested package path"
+    failed=1
+  fi
+done
+
+if [[ "$failed" -ne 0 ]]; then
+  exit 1
+fi
+
+echo "Generated files are in the expected paths."
