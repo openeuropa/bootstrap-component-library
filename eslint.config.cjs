ea5339064228d70cd0d8c8503da40895dea7d769
Dumitru Tiberiu-Andrei <Tiberiu-Andrei.DUMITRU@ext.ec.europa.eu>
2026-06-10 15:31:35 +0300

    OEL-4823: Upgrade eslint from 8 to 10 and adapted.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,56 +1,45 @@
-// eslint.config.cjs
-const { defineFlatConfig } = require("eslint-define-config");
-const babelParser = require("@babel/eslint-parser"); // IMPORTANT: require the parser
+const { defineConfig } = require("eslint/config");
 const jestPlugin = require("eslint-plugin-jest");
 
-module.exports = defineFlatConfig([
+module.exports = defineConfig([
   {
     ignores: [
-      '**/slimselect.min.js',
-      'src/themes/*/js/',
-      'src/themes/*/storybook/bcl-stories'
-    ]
+      "**/slimselect.min.js",
+      "**/slim-select-2/slimselect.min.js",
+      "src/themes/*/js/",
+      "src/themes/*/storybook/bcl-stories",
+    ],
   },
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
 
-    // Must provide an object parser, not just a string
     languageOptions: {
-      parser: babelParser,
-      // parserOptions is where you set Babel parser config
-      parserOptions: {
-        // Tells babel-parser to not require a Babel config file
-        requireConfigFile: false,
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      },
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
-        process: 'readonly',
-        __dirname: 'readonly',
-        module: 'readonly',
-        require: 'readonly',
-        window: 'readonly',
-        document: 'readonly'
-      }
+        process: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+        require: "readonly",
+        window: "readonly",
+        document: "readonly",
+      },
     },
 
     plugins: {
-      jest: jestPlugin
+      jest: jestPlugin,
     },
 
     rules: {
-      'import/no-extraneous-dependencies': 'off',
-      'no-param-reassign': 'off',
-      'import/extensions': 'off',
-      'import/no-webpack-loader-syntax': 'off',
-      'new-cap': 'off',
-      'camelcase': 'off'
-    }
+      "no-param-reassign": "off",
+      "new-cap": "off",
+      camelcase: "off",
+    },
   },
   {
-    files: ['src/**/*.test.js'],
+    files: ["src/**/*.test.js"],
     rules: {
-      'no-undef': 'off'
-    }
-  }
+      "no-undef": "off",
+    },
+  },
 ]);
