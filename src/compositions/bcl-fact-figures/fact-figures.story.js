96bda1cc8e7b220360e1dda8e1d4ea9dccf45b20
Piotr Smykaj <piotrsmykaj@users.noreply.github.com>
2026-09-16 12:20:09 +0200

    OEL-4961: Increase icon size for Facts and figures (#785)
    * OEL-4961: Increase icon size for Facts and figures.
    
    * OEL-4961: Icon size as an option.

--- src/compositions/bcl-fact-figures/fact-figures.story.js
+++ src/compositions/bcl-fact-figures/fact-figures.story.js
@@ -11,6 +11,7 @@ const getArgs = () => ({
   variant: "default",
   alignment: "left",
   responsive_columns: 3,
+  icon_size: "3xl",
   more: true,
   display_icons: true,
 });
@@ -45,6 +46,17 @@ const getArgTypes = () => ({
     table: {
       type: { summary: "boolean" },
       defaultValue: { summary: "true" },
+      category: "Content",
+    },
+  },
+  icon_size: {
+    name: "icon size",
+    type: { name: "select" },
+    options: ["l", "3xl"],
+    description: "Size of the icons",
+    table: {
+      type: { summary: "string" },
+      defaultValue: { summary: "3xl" },
       category: "Content",
     },
   },
